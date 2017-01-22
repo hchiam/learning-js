190d5ee358710125ad2e9d13a1c989b6cbbccec6
hchiam <hchiam@gmail.com>
2017-01-22 13:21:50 -0500

    add another example

--- class.js
+++ class.js
@@ -11,3 +11,16 @@ function myClass(myVar) { // function className(initVars)
 test = new myClass(2); // new className(initVars)
 console.log('test = new myClass(2);');
 console.log('test.get() = ' + test.get());
+
+console.log('\nAnother example:');
+
+function myClassDef(initVar) {
+	this.var = initVar;
+	this.myFun = function(param) {
+		this.var = param + " " + this.var;
+		return this.var;
+	};
+}
+
+myInstance = new myClassDef("Alex");
+console.log(myInstance.myFun("hi"));

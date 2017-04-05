0da47f62a275e089b6ea25b8e5beac1db05414a0
hchiam <hchiam@gmail.com>
2017-04-05 17:21:49 -0400

    add for use as import

---
+++ addTextBasedOnHttpOrHttps.js
@@ -0,0 +1,24 @@
+// I expect to import this code into my projects and have it run right away.
+addTextBasedOnHttpOrHttps();
+
+function addTextBasedOnHttpOrHttps() {
+    let thisUrl = document.URL;
+	if (thisUrl.includes('https')) {
+        // get http link
+    	let urlAsHttpNotHttps = thisUrl.substring(0,4) + thisUrl.substring(5);
+        // create text to add
+    	let addedText = document.createElement('p');
+        addedText.id = 'addTextBasedOnHttpOrHttps';
+      	addedText.appendChild(document.createTextNode('Is this page not working? Maybe try '));
+        // add link to that text
+        let aLink = document.createElement('a');
+        aLink.href = urlAsHttpNotHttps;
+        aLink.innerHTML = 'http';
+        aLink.target = '_blank';
+        addedText.appendChild(aLink);
+        addedText.appendChild(document.createTextNode(' instead of https?'));
+      	document.body.insertBefore(addedText, document.body.firstChild);
+        // center the added text
+        addedText.setAttribute('align','center');
+    }
+}

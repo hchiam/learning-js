7651541d5042eabd4018d36b1098ac255257c96a
Howard T. Chiam <hchiam@users.noreply.github.com>
2018-03-04 11:14:06 -0500

    Create composition.js

---
+++ composition.js
@@ -0,0 +1,29 @@
+// see example: https://codepen.io/hchiam/pen/GQPgQq
+
+const canBark = (data) => ({
+  bark: () => alert(data.name + ': woof')
+})
+
+const canMeow = (data) => ({
+  meow: () => alert(data.name + ': meow')
+})
+
+const catDog = (name) => {
+  let data = {
+    
+  }
+  return Object.assign(
+    {},
+    canBark(name),
+    canMeow(name)
+  )
+}
+
+function create() {
+  const sparky = canBark({name:'sparky'})
+  sparky.bark()
+  // sparky.meow() // won't work
+  const mix = catDog({name:'chimera'})
+  mix.bark()
+  mix.meow()
+}

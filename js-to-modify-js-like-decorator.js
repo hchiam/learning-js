e8ca097e576a23e2e20b1eedb6b2c4f758d6d2a5
Howard T. Chiam <hchiam@users.noreply.github.com>
2019-07-21 11:45:01 -0400

    Create js-to-modify-js-like-decorator.js

---
+++ js-to-modify-js-like-decorator.js
@@ -0,0 +1,15 @@
+// JS to modify JS function like a Python decorator
+// https://stackoverflow.com/questions/33361320/how-to-modify-javascript-function-in-main-js-file
+
+function functionToBeChanged(str){
+    alert(str);
+}
+
+var boringFunction = functionToBeChanged;
+functionToBeChanged = function(str){
+    alert('before');
+    boringFunction.call(this, str); // (you may need to review definition of "this" inside call)
+    alert('after');
+}
+
+functionToBeChanged('hi');

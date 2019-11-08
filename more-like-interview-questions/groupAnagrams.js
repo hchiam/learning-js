2435d3bb851227096dd464f24ef7171dd1623aab
Howard T. Chiam <hchiam@users.noreply.github.com>
2019-11-08 14:25:04 -0500

    slightly even more faster and shorter (no object needed)

--- more-like-interview-questions/groupAnagrams.js
+++ more-like-interview-questions/groupAnagrams.js
@@ -6,89 +6,64 @@
  */
 const groupAnagrams = (strings) => { // O(N s)
   if (strings.length === 0) return [[]];
-  let output = [];
-  const ht = new HT();
-  // O(N s)
-  for (let i = 0; i < strings.length; i++) { // O(N)
-    ht.add(strings[i]); // O(s)
+  const hashTable = new Map();
+  for (const string of strings) { // O(N) * O(s):
+    const hashKey = getHashKey(string); // O(s)
+    // const hashKey = string.split('').sort().join(); // O(s log s) but apparently still faster on leetcode
+    const item = hashTable.get(hashKey) || [];
+    item.push(string);
+    hashTable.set(hashKey, item);
   }
-  for (const key in ht.ht) { // O(N)
-    if (key in ht.ht) { // O(1)
-      if (output.length === 0) {
-        output = [ht.ht[key]];
-      } else {
-        output.push(ht.ht[key]);
-      }
-    }
-  }
-  return output;
+  return Array.from(hashTable.values()); // O(N)
 };
 
-
-class HT {
-  constructor() {
-    this.ht = {};
+function getHashKey(string) { // O(s)
+  const letterCounts = {
+    'a': 0,
+    'b': 0,
+    'c': 0,
+    'd': 0,
+    'e': 0,
+    'f': 0,
+    'g': 0,
+    'h': 0,
+    'i': 0,
+    'j': 0,
+    'k': 0,
+    'l': 0,
+    'm': 0,
+    'n': 0,
+    'o': 0,
+    'p': 0,
+    'q': 0,
+    'r': 0,
+    's': 0,
+    't': 0,
+    'u': 0,
+    'v': 0,
+    'w': 0,
+    'x': 0,
+    'y': 0,
+    'z': 0,
+  };
+  for (let i = 0; i < string.length; i++) { // O(s)
+    const letter = string[i];
+    letterCounts[letter]++;
   }
-
-  add(string) { // O(s)
-    const hashKey = this.getHashKey(string); // O(s)
-    if (this.hasHashKey(hashKey)) { // O(1)
-      this.ht[hashKey].push(string);
-    } else {
-      this.ht[hashKey] = [string];
+  const keys = Object.keys(letterCounts); // O(1) = O(26)
+  let hashKey = '';
+  for (let i = 0; i < keys.length; i++) { // reduce from 26 down to s
+    const letter = keys[i];
+    const count = letterCounts[letter];
+    if (count > 0) {
+      hashKey += letter + count.toString();
     }
   }
-
-  hasHashKey(hashKey) { // O(1)
-    return hashKey in this.ht;
-  }
-
-  getHashKey(string) { // O(s)
-    const letterCounts = {
-      'a': 0,
-      'b': 0,
-      'c': 0,
-      'd': 0,
-      'e': 0,
-      'f': 0,
-      'g': 0,
-      'h': 0,
-      'i': 0,
-      'j': 0,
-      'k': 0,
-      'l': 0,
-      'm': 0,
-      'n': 0,
-      'o': 0,
-      'p': 0,
-      'q': 0,
-      'r': 0,
-      's': 0,
-      't': 0,
-      'u': 0,
-      'v': 0,
-      'w': 0,
-      'x': 0,
-      'y': 0,
-      'z': 0,
-    };
-    for (let i = 0; i < string.length; i++) { // O(s)
-      const letter = string[i];
-      letterCounts[letter]++;
-    }
-    const keys = Object.keys(letterCounts);// O(1) (no sort O(s log s))
-    let hashKey = '';
-    for (let i = 0; i < keys.length; i++) { // O(1) = O(26)
-      const letter = keys[i];
-      const count = letterCounts[letter];
-      if (count > 0) {
-        hashKey += letter + count.toString();
-      }
-    }
-    return hashKey;
-  }
+  return hashKey;
+  // it's still a good idea to turn the letterCounts into a string
+  // even though Map() can take any data type as keys
+  // return JSON.stringify(letterCounts); // O(1) = O(26)
 }
-
 
 const check = (strings, correctAnswer) => {
   const response = groupAnagrams(strings);

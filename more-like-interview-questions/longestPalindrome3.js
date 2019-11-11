d980dc956362c40288c536c1c412b9bae1c14d34
Howard Chiam <hchiam@users.noreply.github.com>
2019-11-11 12:09:17 -0500

    Update longestPalindrome3.js

--- more-like-interview-questions/longestPalindrome3.js
+++ more-like-interview-questions/longestPalindrome3.js
@@ -1,19 +1,19 @@
 /**
+ * Time: O(n^2)
+ * Slower than O(n) Manacher's Algorithm,
+ * but not expected to think of in interview.
  * @param {string} s
  * @return {string}
  */
-const longestPalindrome = (s) => { // still too slow: O(n^2)
+const longestPalindrome = (s) => {
   if (!s || s.length === 0) return '';
   if (s.length === 1) return s;
-  const palindromeIndices = {};
   let maxPalindrome = s[0];
   let tempMaxPalindrome = s[0];
   // aa
   for (let i = 0; i < s.length - 1; i++) {
     if (s[i] === s[i + 1]) {
-      palindromeIndices[i] = true;
-      palindromeIndices[i + 1] = true;
-      tempMaxPalindrome = growPalindrome(s, i, i+1, palindromeIndices);
+      tempMaxPalindrome = growPalindrome(s, i, i+1);
       if (tempMaxPalindrome.length > maxPalindrome.length) {
         maxPalindrome = tempMaxPalindrome;
       }
@@ -22,10 +22,7 @@ const longestPalindrome = (s) => { // still too slow: O(n^2)
   // aba
   for (let i = 0; i < s.length - 2; i++) {
     if (s[i] === s[i + 2]) {
-      palindromeIndices[i] = true;
-      palindromeIndices[i + 1] = true;
-      palindromeIndices[i + 2] = true;
-      tempMaxPalindrome = growPalindrome(s, i, i+2, palindromeIndices);
+      tempMaxPalindrome = growPalindrome(s, i, i+2);
       if (tempMaxPalindrome.length > maxPalindrome.length) {
         maxPalindrome = tempMaxPalindrome;
       }
@@ -38,10 +35,9 @@ const longestPalindrome = (s) => { // still too slow: O(n^2)
  * @param {string} s
  * @param {number} headIndex
  * @param {number} tailIndex
- * @param {object} palindromeIndices
  * @return {string}
  */
-const growPalindrome = (s, headIndex, tailIndex, palindromeIndices) => {
+const growPalindrome = (s, headIndex, tailIndex, ) => {
   let maxPalindrome = s.substring(headIndex, tailIndex + 1);
   let i = headIndex - 1;
   let j = tailIndex + 1;
@@ -49,8 +45,6 @@ const growPalindrome = (s, headIndex, tailIndex, palindromeIndices) => {
     if (s[i] !== s[j]) {
       return maxPalindrome;
     } else {
-      palindromeIndices[i] = true;
-      palindromeIndices[j] = true;
       maxPalindrome = s.substring(i, j + 1);
       i--;
       j++;

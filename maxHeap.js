a156d55bde61fe9bef03c26b489fbba0a5155e70
Howard Chiam <hchiam@users.noreply.github.com>
2019-11-22 21:24:55 -0500

    makes another thing work
    https://github.com/hchiam/learning-js/blob/master/more-like-interview-questions/topKFrequent.js

--- maxHeap.js
+++ maxHeap.js
@@ -16,7 +16,7 @@ const MaxHeap = function() {
     // bubble up:
     if (heap.length > 2) {
       let idx = heap.length - 1;
-      while (heap[idx] > heap[Math.floor(idx/2)]) {
+      while (heap[idx] >= heap[Math.floor(idx/2)]) {
         if (idx >= 1) {
           [heap[Math.floor(idx/2)], heap[idx]] =
             [heap[idx], heap[Math.floor(idx/2)]];

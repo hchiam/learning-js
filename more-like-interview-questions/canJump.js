ba6dc70bc944fc38ace4bec1dc23f39fee153551
hchiam <hchiam@users.noreply.github.com>
2019-11-27 12:35:43 -0500

    add canJump

---
+++ more-like-interview-questions/canJump.js
@@ -0,0 +1,39 @@
+/* eslint-disable require-jsdoc */
+
+// eslint-disable-next-line no-unused-vars
+const canJumpDirectlyDP = (numbers = []) => { // T: O(n v); S: O(n)
+  if (numbers.length < 2) return true;
+  if (numbers[0] === 0) return false;
+  const dp = new Array(numbers.length);
+  dp[0] = true;
+  // go left to right, filling in what can be reached from each number
+  for (let i = 0; i < numbers.length; i++) {
+    if (dp[i] !== true) return false;
+    for (let v = 1; v <= numbers[i] && i + v < numbers.length; v++) {
+      dp[i + v] = true;
+    }
+  }
+  return dp[dp.length - 1] === true;
+};
+
+const canJumpGreedySolution = (numbers = []) => { // T: O(n); S: O(1)
+  if (numbers.length < 2) return true;
+  if (numbers[0] === 0) return false;
+  // observation: can jump from any number > 0 to another adjacent one
+  let iTo = numbers.length-1; // end
+  // go right to left, start trying to get to destination from 1 left
+  for (let iFrom = numbers.length-2; iFrom>=0; iFrom--) {
+    if (iFrom + numbers[iFrom] >= iTo) {
+      iTo = iFrom; // update where you can get to
+    }
+  }
+  return iTo === 0; // whether hit start
+};
+
+function solutionWrapper(input) {
+  return canJumpGreedySolution(input);
+}
+
+module.exports = {
+  solutionWrapper,
+};
---
+++ more-like-interview-questions/canJump.test.js
@@ -0,0 +1,39 @@
+/* eslint-disable require-jsdoc */
+
+const {solutionWrapper} = require('./canJump.js');
+// const {add} = require('./index.js');
+
+describe('index', () => {
+  it('works', () => expect(solutionWrapper([0])).toBe(true));
+  it('works', () => expect(solutionWrapper([1])).toBe(true));
+  it('works', () => expect(solutionWrapper([1, 0])).toBe(true));
+  it('works', () => expect(solutionWrapper([0, 1])).toBe(false));
+  it('works', () => expect(solutionWrapper([0, 0])).toBe(false));
+  it('works', () => expect(solutionWrapper([1, 0, 4])).toBe(false));
+  it('works', () => expect(solutionWrapper([2, 0, 4])).toBe(true));
+  it('works', () => expect(solutionWrapper([1, 2, 3, 4])).toBe(true));
+  it('works', () => expect(solutionWrapper([4, 3, 2, 1])).toBe(true));
+  it('works', () => expect(solutionWrapper([1, 1, 1, 1, 2, 0, 3])).toBe(true));
+  it('works', () => expect(solutionWrapper([3, 1, 1, 2, 0, 5])).toBe(true));
+  it('works', () => expect(solutionWrapper([3, 1, 1, 1, 0, 5])).toBe(false));
+  it('works', () => expect(solutionWrapper([2, 0, 1, 0, 4])).toBe(false));
+  it('works', () => expect(solutionWrapper([1, 0, 2, 0, 4])).toBe(false));
+  it('works', () => expect(solutionWrapper([2, 0, 2, 0, 4])).toBe(true));
+  it('works', () => expect(solutionWrapper([5, 4, 3, 2, 1, 0, 4])).toBe(false));
+  it('works', () => expect(solutionWrapper([5, 4, 3, 2, 2, 0, 4])).toBe(true));
+  it('works', () => expect(solutionWrapper([5, 4, 3, 2, 0, 4])).toBe(true));
+  it('works', () => expect(solutionWrapper(
+      [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0])).toBe(true));
+  it('works', () => expect(solutionWrapper([3, 0, 0, 0])).toBe(true));
+  it('works', () => expect(solutionWrapper(
+      [2, 0, 0, 0, 2, 0, 0, 0])).toBe(false));
+  it('works', () => expect(solutionWrapper(
+      [7, 0, 2, 1, 0, 8])).toBe(true));
+  it('works', () => expect(solutionWrapper(
+      [8, 2, 4, 4, 4, 9, 5, 2, 5, 8, 8, 0, 8, 6, 9, 1, 1, 6, 3, 5,
+        1, 2, 6, 6, 0, 4, 8, 6, 0, 3, 2, 8, 7, 6, 5, 1, 7, 0, 3, 4,
+        8, 3, 5, 9, 0, 4, 0, 1, 0, 5, 9, 2, 0, 7, 0, 2, 1, 0, 8, 2,
+        5, 1, 2, 3, 9, 7, 4, 7, 0, 0, 1, 8, 5, 6, 7, 5, 1, 9, 9, 3,
+        5, 0, 7, 5]
+  )).toBe(true));
+});

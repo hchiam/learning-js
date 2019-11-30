d93e99aa18032e360eb99fde19bc959577c4cdf0
hchiam <hchiam@users.noreply.github.com>
2019-11-29 22:08:15 -0500

    add getIntersectionNode (3)

---
+++ more-like-interview-questions/getIntersectionNode3.js
@@ -0,0 +1,57 @@
+/* eslint-disable require-jsdoc */
+
+// apparently the 3rd time I tried this question already:
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/107/linked-list/785/
+
+// to test this file, you can run this command in CLI:
+// jest getIntersectionNode3.test.js
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *   this.val = val;
+ *   this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = (headA, headB) => {
+  if (headA === headB) return headA;
+  let pHeadA = headA;
+  let pHeadB = headB;
+  let pA = headA;
+  let pB = headB;
+  while (pA !== null && pB !== null) {
+    pA = pA.next;
+    pB = pB.next;
+  }
+  while (pA !== null) {
+    pA = pA.next;
+    pHeadA = pHeadA.next;
+  }
+  while (pB !== null) {
+    pB = pB.next;
+    pHeadB = pHeadB.next;
+  }
+  while (pHeadA !== null && pHeadB !== null) {
+    pHeadA = pHeadA.next;
+    pHeadB = pHeadB.next;
+    if (pHeadA === pHeadB) {
+      return pHeadA;
+    }
+  }
+  // otherwise no intersection
+  return null;
+};
+
+function solutionWrapper(a, b) {
+  return getIntersectionNode(a, b);
+}
+
+module.exports = {
+  solutionWrapper,
+};
---
+++ more-like-interview-questions/getIntersectionNode3.test.js
@@ -0,0 +1,44 @@
+/* eslint-disable require-jsdoc */
+
+const {solutionWrapper} = require('./getIntersectionNode3.js');
+// const {add} = require('./index.js');
+
+describe('case 1', () => {
+  const a = new ListNode(4);
+  a.next = new ListNode(1);
+  a.next.next = new ListNode(8);
+  a.next.next.next = new ListNode(4);
+  a.next.next.next.next = new ListNode(5);
+  const b = new ListNode(5);
+  b.next = new ListNode(0);
+  b.next.next = new ListNode(1);
+  b.next.next.next = a.next.next;
+  it('works', () => expect(solutionWrapper(a, b)).toBe(a.next.next));
+  it('works', () => expect(solutionWrapper(a, b)).toBe(b.next.next.next));
+});
+
+describe('case 2', () => {
+  const a = new ListNode(0);
+  a.next = new ListNode(9);
+  a.next.next = new ListNode(1);
+  a.next.next.next = new ListNode(2);
+  a.next.next.next.next = new ListNode(4);
+  const b = new ListNode(3);
+  b.next = a.next.next.next;
+  it('works', () => expect(solutionWrapper(a, b)).toBe(a.next.next.next));
+  it('works', () => expect(solutionWrapper(a, b)).toBe(b.next));
+});
+
+describe('case 3', () => {
+  const a = new ListNode(2);
+  a.next = new ListNode(6);
+  a.next.next = new ListNode(4);
+  const b = new ListNode(1);
+  b.next = new ListNode(5);
+  it('works', () => expect(solutionWrapper(a, b)).toBe(null));
+});
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}

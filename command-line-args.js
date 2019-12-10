baad0799f55ec1a5eca36e37a8a41cba609d4271
hchiam <hchiam@users.noreply.github.com>
2019-12-10 12:06:20 -0500

    add command line args example

---
+++ command-line-args.js
@@ -0,0 +1,18 @@
+/*
+You can use this JavaScript file in command line CLI. 
+
+For example, enter this command: 
+node command-line-args.js param1 param2=something
+
+The key thing to remember is this array: process.argv
+*/
+
+var arrayOfArguments = process.argv;
+console.log('arrayOfArguments:', arrayOfArguments);
+
+// first arg: path to node
+// second arg: path to current .js file
+// later args: what you typed afterwards in the command, separated by spaces
+process.argv.slice(2).forEach(function (rawArgumentString) {
+  console.log(rawArgumentString + ' (' + typeof rawArgumentString + ')');
+});

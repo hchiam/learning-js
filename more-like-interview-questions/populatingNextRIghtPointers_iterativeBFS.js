35fb4b0d0984c7d9c0d675b5123f22deae581174
Howard Chiam <hchiam@users.noreply.github.com>
2019-12-27 21:40:01 -0500

    Create populatingNextRIghtPointers_iterativeBFS.js

---
+++ more-like-interview-questions/populatingNextRIghtPointers_iterativeBFS.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) { // iterative, BFS
+    const noMoreLevels = (root === null || root.left === null);
+    if (noMoreLevels) return root;
+    const q = [root];
+    while (q.length > 0) {
+        const n = q.shift();
+        const hitBottomOfPerfectBinaryTree = !n.left;
+        if (hitBottomOfPerfectBinaryTree) {
+            break;
+        }
+        n.left.next = n.right;
+        const canGiveRightChildNext = n.next;
+        if (canGiveRightChildNext) {
+            n.right.next = n.next.left;
+        }
+        q.push(n.left);
+        q.push(n.right);
+    }
+    return root;
+};

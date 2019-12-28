a764a511f53831a23a648baf4a031fa13ec4b96a
Howard Chiam <hchiam@users.noreply.github.com>
2019-12-27 21:39:11 -0500

    Create populatingNextRightPointers_recursive.js

---
+++ more-like-interview-questions/populatingNextRightPointers_recursive.js
@@ -0,0 +1,25 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) { // recursive, DFS
+    const noMoreLevelsInPerfectBinaryTree = (root === null || root.left === null);
+    if (noMoreLevelsInPerfectBinaryTree) return root;
+    root.left.next = root.right;
+    const canGiveRightChildNext = root.next;
+    if (canGiveRightChildNext) {
+        root.right.next = root.next.left;
+    }
+    connect(root.left);
+    connect(root.right);
+    return root;
+};

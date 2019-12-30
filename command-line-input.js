eb3340f04c66d14e6d0c7d5d1967d0324c3c9694
Howard Chiam <hchiam@users.noreply.github.com>
2019-12-30 13:19:47 -0500

    Create command-line-input.js

---
+++ command-line-input.js
@@ -0,0 +1,29 @@
+/*
+You can use this JavaScript file in command line CLI.
+For example, enter this command:
+node command-line-input.js
+Key things to remember: process.openStdin()/exit() and addListener('data',...)
+*/
+
+const hi = () => {
+  console.log(`Hi! This is a CLI app written in JS.
+
+To exit, hit Ctrl+C or Command+C.
+
+Enter something:`);
+};
+
+hi();
+
+const stdin = process.openStdin();
+stdin.addListener('data', (data) => {
+  const line = data.toString().trim(); // remove newline at end
+  const userSaidBye = line.match(/[good ?]?bye/i);
+  console.log(`\n You entered: ${line}\n`);
+  if (!userSaidBye) {
+    console.log('Say something again:');
+  } else {
+    console.log('Bye!\n');
+    process.exit();
+  }
+});

bf2cff70efa2da912dd702c7419dbd6e99623667
Howard Chiam <hchiam@users.noreply.github.com>
2020-01-01 12:37:29 -0500

    add permutations

---
+++ more-like-interview-questions/permute.js
@@ -0,0 +1,50 @@
+/* eslint-disable require-jsdoc */
+
+/**
+ * @param {number[]} nums
+ * @return {[number[]]}
+ */
+const permute = (nums) => {
+  if (!nums || nums.length < 1) return [[]];
+
+  const output = new Set();
+
+  const recursivelyGetPermutations = (nums, combo=[]) => {
+    if (nums.length === 1) {
+      combo.push(nums[0]);
+      output.add(combo);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      const newNums = nums.slice(); // avoid affecting original
+      const useThis = newNums.splice(i, 1)[0];
+      const isNewCombo = (combo.indexOf(useThis) === -1);
+      if (isNewCombo) {
+        const newCombo = combo.slice(); // avoid affecting original
+        newCombo.push(useThis);
+        recursivelyGetPermutations(newNums, newCombo);
+      }
+    }
+  };
+
+  recursivelyGetPermutations(nums, []);
+  return Array.from(output);
+};
+
+function solutionWrapper(params) {
+  return permute(params);
+}
+
+// function ListNode(val) {
+//   this.val = val;
+//   this.next = null;
+// }
+
+// function TreeNode(val) {
+//   this.val = val;
+//   this.left = this.right = null;
+// }
+
+module.exports = {
+  solutionWrapper,
+};
---
+++ more-like-interview-questions/permute.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable require-jsdoc */
+
+const {solutionWrapper} = require('./index.js');
+
+describe('the solution handles silly input', () => {
+  it('works', () => expect(solutionWrapper()).toStrictEqual([[]]));
+  it('works', () => expect(solutionWrapper('')).toStrictEqual([[]]));
+  it('works', () => expect(solutionWrapper([])).toStrictEqual([[]]));
+  it('works', () => expect(solutionWrapper([1])).toStrictEqual([[1]]));
+});
+
+describe('the solution', () => {
+  it('works with 2', () => expect(solutionWrapper([1, 2]))
+      .toStrictEqual([[1, 2], [2, 1]]));
+  it('works with 3', () => expect(solutionWrapper([1, 2, 3]))
+      .toStrictEqual([[1, 2, 3], [1, 3, 2], [2, 1, 3],
+        [2, 3, 1], [3, 1, 2], [3, 2, 1]]));
+});
+
+// function ListNode(val) {
+//   this.val = val;
+//   this.next = null;
+// }
+
+// function TreeNode(val) {
+//   this.val = val;
+//   this.left = this.right = null;
+// }

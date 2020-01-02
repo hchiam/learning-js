f06ac6115226eb8eb347646b31dfcf9e8076dd8c
Howard Chiam <hchiam@users.noreply.github.com>
2020-01-02 12:30:50 -0500

    Map() is faster than {}, and has more readable functions too

--- more-like-interview-questions/lengthOfLongestSubstring_2.js
+++ more-like-interview-questions/lengthOfLongestSubstring_2.js
@@ -7,17 +7,18 @@
 const lengthOfLongestSubstring = (s) => {
   if (!s || s.length < 1) return 0;
   if (s.length === 1) return 1;
-  let headIndex = 0; // ++ or h[s[i]]+1 when same
+  let headIndex = 0; // ++ or ht.get(s[i])+1 when same
   let maxLength = 1;
   let tempLength = 0; // = diff+1 with headIndex when same
-  const ht = {}; // {letter: index}
+  const ht = new Map(); // {letter: index}
   for (let i = 0; i < s.length; i++) { // O(s)
-    if (s[i] in ht) {
-      headIndex = Math.max(ht[s[i]]+1, headIndex);
+    if (ht.has(s[i])) {
+      headIndex = Math.max(ht.get(s[i])+1, headIndex);
       // do Math.max with headIndex in case it's before or after headIndex
-      // ht[s[i]]+1 covers the case where it's also at headIndex
+      // ht.get(s[i])+1 covers the case where it's also at headIndex
     }
-    ht[s[i]] = i; // update if not have already, & update if already have too
+    // update if not have already, & update if already have too
+    ht.set(s[i], i);
     tempLength = i - headIndex + 1;
     maxLength = Math.max(maxLength, tempLength);
   }

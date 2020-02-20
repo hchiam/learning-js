be58f9ce24cb14be5fd1eeb63ec187c6fb29d88d
hchiam <hchiam@gmail.com>
2020-02-20 14:33:15 -0500

    add min stack

---
+++ minStack.js
@@ -0,0 +1,60 @@
+/**
+ * https://leetcode.com/explore/interview/card/top-interview-questions-easy/98/design/562/
+ * Stack that supports push/pop/top(peek) but also min in constant time.
+ * Hint: NO NEED FOR A MIN HEAP!
+ */
+
+/**
+ * initialize your data structure here.
+ */
+const MinStack = function() {
+  this.stack = []; // format: [{value, minUpToMe}, ...]
+};
+
+/**
+* @param {number} x
+* @return {void}
+*/
+MinStack.prototype.push = function(x) {
+  if (this.stack.length === 0) {
+    this.stack.push({value: x, minUpToMe: x});
+  } else {
+    const previousMin = this.stack[this.stack.length - 1].minUpToMe;
+    const minUpToMe = Math.min(previousMin, x);
+    this.stack.push({value: x, minUpToMe: minUpToMe});
+  }
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function() {
+  return this.stack.pop().value;
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function() {
+  return this.stack[this.stack.length - 1].value;
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function() {
+  return this.stack[this.stack.length - 1].minUpToMe;
+};
+
+/**
+* Your MinStack object will be instantiated and called as such:
+* var obj = new MinStack()
+* obj.push(x)
+* obj.pop()
+* var param_3 = obj.top()
+* var param_4 = obj.getMin()
+*/
+
+module.exports = {
+  MinStack,
+};
---
+++ minStack.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable require-jsdoc */
+
+const {MinStack} = require('./minStack.js');
+
+describe('the solution', () => {
+  it('works', () => {
+    const minStack = new MinStack();
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    const min1 = minStack.getMin();
+    const pop = minStack.pop();
+    const top = minStack.top();
+    const min2 = minStack.getMin();
+    expect(min1).toStrictEqual(-3);
+    expect(pop).toStrictEqual(-3);
+    expect(top).toStrictEqual(0);
+    expect(min2).toStrictEqual(-2);
+  });
+});

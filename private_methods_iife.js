c1cc14f3763fb23a44dda375d856cd5e8ca53934
Howard Chiam <hchiam@users.noreply.github.com>
2020-03-04 18:20:22 -0500

    with example from https://www.w3schools.com/js/js_function_closures.asp

--- private_methods_iife.js
+++ private_methods_iife.js
@@ -15,3 +15,19 @@ let app = (function () {
 var a = new app();
 a.sayHi();
 alert('The private local property "name" should be undefined: ' + (typeof a.name == 'undefined'));
+
+
+// another example with function closures, from: https://www.w3schools.com/js/js_function_closures.asp
+
+var add = (function () {
+  var counter = 0; // "private" variable only accessible whenever you call add
+  return function add_theReturnedFunction() {
+    counter += 1; // update the "private" variable that's only accessible inside the IIFE
+    alert('The "private" counter = ' + counter);
+    return counter;
+  }
+})();
+
+add();
+add();
+add();

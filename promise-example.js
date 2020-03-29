12f8ad6883ffe5c5757fdc8bc8bb6dfcf0a1927b
Howard Chiam <hchiam@users.noreply.github.com>
2020-03-29 13:44:33 -0400

    Update promise-example.js

--- promise-example.js
+++ promise-example.js
@@ -8,3 +8,15 @@ test().then(function(result) {
     console.log(result);
     alert(result);
 });
+
+// and promise chaining;
+
+new Promise(function(resolve, reject) {
+  setTimeout(() => resolve('output 1'), 1000);
+}).then(function(result) {
+  alert(result); // 'output 1'
+  return 'output 2';
+}).then(function(result) {
+  alert(result); // 'output 2'
+  return 'output 3';
+});

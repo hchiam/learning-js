24ba6f649e989b7ca43b642348c7c699ee1aedb1
Howard Chiam <hchiam@users.noreply.github.com>
2020-04-07 23:48:57 -0400

    add clarifying comment

--- generator-another-example.js
+++ generator-another-example.js
@@ -6,8 +6,8 @@ function *myGenerator() {
 }
 
 var it = myGenerator();
-it.next(); // first call of .next does not do anything yet
-it.next('some value I can set the yield to assign to x');
+it.next(); // first call of .next() does not do anything yet because it stops at the yield until the next .next():
+it.next('some value I can set the yield to assign to x'); // x = '...' and now runs console.log
 it.next('another value like it');
 it.next('and again');
 it.next(); // undefined

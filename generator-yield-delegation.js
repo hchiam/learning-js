e9ef71f976e3e12855f193aa015943c1eaf00a67
Howard Chiam <hchiam@users.noreply.github.com>
2020-04-08 00:11:51 -0400

    Create generator-yield-delegation.js

---
+++ generator-yield-delegation.js
@@ -0,0 +1,25 @@
+function *generator1() {
+  yield 1;
+  yield 2;
+  return 4;
+}
+
+function normalFunctionReturningAnIterable() {
+  return [5, 6, 7];
+}
+
+function *generator2() {
+  const val = yield *generator1(); // <- here be yield delegation to another generator
+  yield 3;
+  yield val;
+  yield *normalFunctionReturningAnIterable(); // <- here be yield delegation to an iterator
+}
+
+const it = generator2();
+console.log(it.next().value); // 1
+console.log(it.next().value); // 2
+console.log(it.next().value); // 3
+console.log(it.next().value); // 4
+console.log(it.next().value); // 5
+console.log(it.next().value); // 6
+console.log(it.next().value); // 7

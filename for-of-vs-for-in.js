6021e2dd1cdd73af01ceb26c77cad0b14c6c22df
Howard Chiam <hchiam@users.noreply.github.com>
2020-06-05 19:38:30 -0400

    Create for-of-vs-for-in.js

---
+++ for-of-vs-for-in.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3];
+Array.prototype.propThatShouldNotBeReadable = 'whoops!';
+
+// for...in incorrectly prints 'whoops!':
+for (let i in arr) {
+  console.log(arr[i]);
+}
+
+// for...of does not:
+for (const value of arr) {
+  console.log(value);
+}
+
+// so always use for...of, and you can get both keys and values of an array by doing this:
+for (const [key, value] of Object.entries(arr)) {
+  console.log(key, value);
+}

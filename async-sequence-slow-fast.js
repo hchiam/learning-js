bef6940b1d1dc2a4e020c5c42a0bfea9097dad7e
Howard Chiam <hchiam@users.noreply.github.com>
2020-06-13 22:40:24 -0400

    Create async-sequence-slow-fast.js

---
+++ async-sequence-slow-fast.js
@@ -0,0 +1,34 @@
+let timeTest = timeTest_fast; // timeTest_slow;
+
+let startTime = Date.now();
+timeTest().then(() => {
+  let finishTime = Date.now();
+  let timeTaken = finishTime - startTime;
+  alert("Time taken in milliseconds: " + timeTaken);
+});
+
+async function timeTest_slow() {
+  // ~9000
+  await timeoutPromise(3000);
+  await timeoutPromise(3000);
+  await timeoutPromise(3000);
+}
+
+async function timeTest_fast() {
+  // ~3000
+  const timeoutPromise1 = timeoutPromise(3000);
+  const timeoutPromise2 = timeoutPromise(3000);
+  const timeoutPromise3 = timeoutPromise(3000);
+
+  await timeoutPromise1;
+  await timeoutPromise2;
+  await timeoutPromise3;
+}
+
+function timeoutPromise(interval) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      resolve("done");
+    }, interval);
+  });
+}

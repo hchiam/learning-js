e80d0cd4d64936b2a26a64067b6074054d3287a5
Howard Chiam <hchiam@users.noreply.github.com>
2020-06-19 20:32:07 -0400

    Create copy-element-to-clipboard.js

---
+++ copy-element-to-clipboard.js
@@ -0,0 +1,7 @@
+// Try this in the console panel of your browser!
+copy(document.body);
+
+// You can also copy JS objects to your clipboard:
+copy({ my: 'object' });
+
+// https://umaar.com/dev-tips/225-copy-html-element-clipboard

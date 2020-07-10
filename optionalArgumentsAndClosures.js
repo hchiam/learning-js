4c424fb5d442258e17b19814e69ec41118696b11
hchiam <hchiam@users.noreply.github.com>
2020-07-10 14:12:53 -0400

    autoformatted, console.log, var

--- optionalArgumentsAndClosures.js
+++ optionalArgumentsAndClosures.js
@@ -1,20 +1,20 @@
 function addTogether() {
   var sum = 0;
-  
+
   // go through each item in arguments
-  for(i=0; i<arguments.length;i++){
+  for (var i = 0; i < arguments.length; i++) {
     if (typeof arguments[i] !== "number") return undefined;
     sum += arguments[i];
   }
-  
+
   // e.g. if addTogether(2,4) :
   if (arguments.length !== 1) {
     return sum;
-    
-  } else { // e.g. if addTogether(1) :
-    return function() {
-      for(i=0; i<arguments.length;i++){
-      if (typeof arguments[i] !== "number") return undefined;
+  } else {
+    // e.g. if addTogether(1) :
+    return function () {
+      for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] !== "number") return undefined;
         sum += arguments[i];
       }
       return sum;
@@ -22,4 +22,4 @@ function addTogether() {
   }
 }
 
-addTogether(1)(2,4); // should return 7
+console.log(addTogether(1)(2, 4)); // should return 7

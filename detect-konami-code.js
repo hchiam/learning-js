99b3b87e918791ce7d2a44b748fc266f54411f4a
hchiam <hchiam@users.noreply.github.com>
2020-07-19 19:46:17 -0400

    add detect-konami-code.js

---
+++ detect-konami-code.js
@@ -0,0 +1,26 @@
+// reference: https://www.sitepoint.com/jquery-konami-code-listener
+
+function onKonamiCode() {
+  alert("Konami code detected!");
+}
+
+(function () {
+  if (window.addEventListener) {
+    var seq = [];
+    var konamiAsciiSequence = "38,38,40,40,37,39,37,39,66,65";
+    window.addEventListener(
+      "keydown",
+      function (event) {
+        var key = event.which || event.keyCode;
+        seq.push(key);
+        var hitKonamiSequence =
+          seq.toString().indexOf(konamiAsciiSequence) >= 0;
+        if (hitKonamiSequence) {
+          onKonamiCode();
+          seq = [];
+        }
+      },
+      true
+    );
+  }
+})();

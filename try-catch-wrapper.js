d47d0d4f80067c73661d774e8fd063678a331fe1
Howard Chiam <hchiam@users.noreply.github.com>
2020-07-29 00:26:44 -0400

    Create try-catch-wrapper.js

---
+++ try-catch-wrapper.js
@@ -0,0 +1,22 @@
+function tryCatchWrapper(mainFunction, catchFunction, ...args) {
+  try {
+    mainFunction(...args);
+  } catch(e) {
+    if (catchFunction) {
+      catchFunction
+    } else {
+      console.log('This is a caught error:\n\n', e);
+    }
+  }
+}
+
+// example usage:
+
+var a = 'some input outside the function';
+
+tryCatchWrapper(function mainFunction(...inputs) {
+  alert(a);
+  alert(inputs)
+}, function catchFunction() {
+  alert('custom catch override')
+}, 'some custom input');

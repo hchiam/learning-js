df1f8a5592018a1d4c4a97bc3de6fe1c46c9cd23
Howard Chiam <hchiam@users.noreply.github.com>
2020-08-05 21:30:31 -0400

    add links for more

--- spread-operator.js
+++ spread-operator.js
@@ -2,3 +2,7 @@ var a = [1, 2, 3];
 console.log(a);
 console.log(...a);
 console.log(1, 2, 3);
+
+// https://github.com/hchiam/learning-js/blob/master/destructuring-assignment-or-named-optional-parameters.js
+
+// more: https://thenextweb.com/syndication/2020/07/25/how-to-write-cleaner-code-with-javascript/

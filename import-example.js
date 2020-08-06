969bcca17a5cc52ace55ad4230889cdf90832c79
Howard Chiam <hchiam@users.noreply.github.com>
2020-08-05 21:28:30 -0400

    add require example

--- import-example.js
+++ import-example.js
@@ -1,3 +1,8 @@
 // https://stackoverflow.com/questions/39282253/how-can-i-alias-a-default-import-in-javascript
 // import alias from 'my-module';
 // import {default as alias} from 'my-module';
+
+let somethingToImport;
+if (typeof require !== "undefined") {
+  somethingToImport = require("some_package").somethingToImport;
+}

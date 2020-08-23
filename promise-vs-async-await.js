edd276f1da2f5219191ebc280858fb9506321ad7
Howard Chiam <hchiam@users.noreply.github.com>
2020-08-23 14:49:55 -0400

    Create promise-vs-async-await.js

---
+++ promise-vs-async-await.js
@@ -0,0 +1,16 @@
+tryAsyncAwait();
+tryPromise();
+
+function tryPromise() { 
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(data => console.log(data.map(x=>x.title)));
+  // downside: you're already nesting things inside .then
+};
+
+async function tryAsyncAwait() { 
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+  const json = await response.json();
+  console.log(json.map(x=>x.title));
+  // note: canNOT do await fetch(...).json();
+};

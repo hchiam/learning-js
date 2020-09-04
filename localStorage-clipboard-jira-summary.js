2411c42c94deac20b2adfb10d1cd964f1ceeff0b
Howard Chiam <hchiam@users.noreply.github.com>
2020-09-03 20:36:33 -0400

    2 spaces :)

--- localStorage-clipboard-jira-summary.js
+++ localStorage-clipboard-jira-summary.js
@@ -1,12 +1,12 @@
 // stores a function in localStorage that uses jQuery to put together a Jira ticket/subtask summary into your clipboard:
 
 localStorage.setItem('getShelvesetComment', `function getShelvesetComment() {
-    var storyIdText = $('#parent_issue_summary').text();
-    var start = storyIdText ? storyIdMatch.match(/^CD-\\d+/)[0] + ' / ' : '';
-    var comment = start + $('#key-val').text() + ' : ' + $('#summary-val').text();
-    // alert(comment);
-    console.log(comment);
-    return comment;
+  var storyIdText = $('#parent_issue_summary').text();
+  var start = storyIdText ? storyIdMatch.match(/^CD-\\d+/)[0] + ' / ' : '';
+  var comment = start + $('#key-val').text() + ' : ' + $('#summary-val').text();
+  // alert(comment);
+  console.log(comment);
+  return comment;
 }
 var shelvesetComment = getShelvesetComment();
 copy(shelvesetComment);`)

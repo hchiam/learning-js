2b1ea6a73ee996f05772e276c8f90296fede556e
Howard Chiam <hchiam@users.noreply.github.com>
2020-09-18 22:37:24 -0400

    Create console-time.js

---
+++ console-time.js
@@ -0,0 +1,9 @@
+console.time('some name');
+// ...
+console.timeEnd('some name');
+
+/* 
+ * You should see printed in the console 
+ * something like this: 
+ * "some name: ###ms - timer ended"
+ */

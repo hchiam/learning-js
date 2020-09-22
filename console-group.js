167f605db6f74b9a630cc9a9e3654fc28ca5b3ae
Howard Chiam <hchiam@users.noreply.github.com>
2020-09-21 22:48:52 -0400

    correction

--- console-group.js
+++ console-group.js
@@ -3,6 +3,6 @@ console.log('this shows outside of the collapsible group');
 console.groupCollapsed('group name (click to expand)');
   console.log('something inside group');
   console.log('something else inside the same group');
-console.groupEnd('group name (click to expand)'); // has to be the same name to stop adding things under the same group
+console.groupEnd();
 
 console.log('this shows outside of the collapsible group');

b5454abfa6599a75064ed7df0d3766bd0e8e2ab1
hchiam <hchiam@users.noreply.github.com>
2020-10-06 18:36:40 -0400

    example of 2d array using new Array.fill

--- array-fill.js
+++ array-fill.js
@@ -2,3 +2,11 @@ var numSpaces = 10;
 var withWhat = 3;
 console.log(Array(numSpaces).fill(withWhat));
 // [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+
+var rows = 2;
+var cols = 4;
+var val = false;
+var array2d = new Array(rows).fill(new Array(cols).fill(val));
+console.log(array2d);
+console.log(JSON.stringify(array2d));
+// [[false,false,false,false], [false,false,false,false]]

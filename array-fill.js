cb4af6afac9eb0c194c5bc75213771afef72872b
hchiam <hchiam@users.noreply.github.com>
2020-10-06 20:47:22 -0400

    avoid bad referencing in making 2d array

--- array-fill.js
+++ array-fill.js
@@ -3,10 +3,17 @@ var withWhat = 3;
 console.log(Array(numSpaces).fill(withWhat));
 // [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 
-var rows = 2;
-var cols = 4;
+var rows = 3;
+var cols = 5;
 var val = false;
-var array2d = new Array(rows).fill(new Array(cols).fill(val));
+var array2d_BAD = new Array(rows).fill(new Array(cols).fill(val));
+var array2d = new Array(rows).fill(null).map(() => new Array(cols).fill(false));
 console.log(array2d);
 console.log(JSON.stringify(array2d));
-// [[false,false,false,false], [false,false,false,false]]
+// [[false,false,false,false,false],[false,false,false,false,false],[false,false,false,false,false]]
+console.log(array2d_BAD);
+array2d_BAD[0][1] = true;
+console.log(
+  "array2d_BAD is bad because editing one row edits all rows due to referencing:"
+);
+console.log(array2d_BAD);

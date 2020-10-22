b49cf3070b773f5a4493e7e6c9ebc4806a0c25ab
hchiam <hchiam@users.noreply.github.com>
2020-10-21 22:07:40 -0400

    create public-private.js

---
+++ public-private.js
@@ -0,0 +1,44 @@
+function SomeClass() {
+  // private members:
+  let privateVariable = "hidden";
+  function privateFunction() {
+    console.log("you called a private function");
+    return "hidden";
+  }
+
+  // public members:
+  this.publicVariable = "exposed";
+  this.publicFunction = function () {
+    console.log("you called a public static function");
+    return "exposed";
+  };
+}
+
+// public prototype members:
+SomeClass.prototype.publicPrototypeVariable = "exposed";
+SomeClass.prototype.publicPrototypeFunction = function () {
+  console.log("you called a public function");
+  return "exposed";
+};
+
+// use it all:
+
+const obj = new SomeClass();
+const obj2 = new SomeClass();
+
+console.log(obj.privateVariable);
+// obj.privateFunction();
+
+console.log(obj.publicVariable);
+obj.publicFunction();
+
+console.log(obj.publicPrototypeVariable);
+obj.publicPrototypeFunction();
+
+obj.publicVariable = "edited public static variable";
+console.log(obj.publicVariable);
+console.log("should not affect obj2:", obj2.publicVariable);
+
+obj.publicPrototypeVariable = "edited public variable";
+console.log(obj.publicPrototypeVariable);
+console.log("should not affect obj2:", obj2.publicPrototypeVariable);

096273531e0ca743238dd247388227ab997477b7
hchiam <hchiam@users.noreply.github.com>
2020-10-22 00:04:37 -0400

    account for which and keyCode deprecated

--- detect-konami-code.js
+++ detect-konami-code.js
@@ -1,5 +1,7 @@
 // reference: https://www.sitepoint.com/jquery-konami-code-listener
 
+// https://stackoverflow.com/questions/35394937/keyboardevent-keycode-deprecated-what-does-this-mean-in-practice
+
 function onKonamiCode() {
   alert("Konami code detected!");
 }
@@ -8,14 +10,17 @@ function onKonamiCode() {
   if (window.addEventListener) {
     var seq = [];
     var konamiAsciiSequence = "38,38,40,40,37,39,37,39,66,65";
+    var sequenceInWords =
+      "ArrowUp,ArrowUp,ArrowDown,ArrowDown,ArrowLeft,ArrowRight,ArrowLeft,ArrowRight,b,a";
     window.addEventListener(
       "keydown",
       function (event) {
-        var key = event.which || event.keyCode;
+        var key = event.key || event.which || event.keyCode;
         seq.push(key);
         var hitKonamiSequence =
           seq.toString().indexOf(konamiAsciiSequence) >= 0;
-        if (hitKonamiSequence) {
+        var hitWordSequence = seq.toString().indexOf(sequenceInWords) >= 0;
+        if (hitKonamiSequence || hitWordSequence) {
           onKonamiCode();
           seq = [];
         }

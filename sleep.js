615e7d5915089d56fd7ce66a44e0ac347b2f1c7b
hchiam <hchiam@users.noreply.github.com>
2020-11-10 19:55:07 -0500

    another example usage

--- sleep.js
+++ sleep.js
@@ -8,11 +8,30 @@ async function sleep(milliseconds) {
   await new Promise((r) => setTimeout(r, milliseconds || 1000));
 }
 
+// example usage:
+
 (async function () {
   console.log("run 1 thing and wait 2 seconds");
   await sleep(2000);
   console.log("run 2nd thing after 2 seconds");
 })();
+
+// or:
+
+test();
+function s(selector) {
+  const result = $(selector);
+  if (result.is(":visible:not(:disabled)")) return result;
+}
+async function test() {
+  s("#button_1").click();
+  await sleep();
+  s("#input_2").val(123);
+  s("#button_3").click();
+  await sleep(2000);
+  s("#button_4").click();
+  alert("done");
+}
 
 // https://stackoverflow.com/questions/17574571/nested-settimeout-alternative
 

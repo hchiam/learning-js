36cb2d91080b1b2fa1a0785fd323603579449b31
Howard Chiam <hchiam@users.noreply.github.com>
2021-01-06 21:57:54 -0500

    browser vs console terminal

--- read-json-file.js
+++ read-json-file.js
@@ -1,3 +1,7 @@
+// in the browser:
+
 fetch("data.json")
   .then((response) => response.json())
   .then((jsonResponse) => console.log(jsonResponse));
+
+// in console terminal CLI: https://github.com/hchiam/learning-nodejs/blob/master/README.md#read-a-local-file-via-console-cli

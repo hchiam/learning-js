40fd84abc1b5d5eef865b9cc4c4cbe25adc019e7
Howard Chiam <hchiam@users.noreply.github.com>
2021-01-06 21:57:52 -0500

    browser vs console terminal

--- read-text-file.js
+++ read-text-file.js
@@ -1,3 +1,7 @@
+// in the browser:
+
 fetch("file.txt")
   .then((response) => response.text())
   .then((text) => console.log(text));
+
+// in console terminal CLI: https://github.com/hchiam/learning-nodejs/blob/master/README.md#read-a-local-file-via-console-cli

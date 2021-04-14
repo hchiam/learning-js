3f5be1812a6bba81b60bb7bc7f62d59a208d11a5
hchiam <hchiam@users.noreply.github.com>
2021-04-13 20:23:43 -0400

    .text... -> .content... works better in both demo and live page

--- bookmarklets/nearpod-sticky-notes-scraper.js
+++ bookmarklets/nearpod-sticky-notes-scraper.js
@@ -1,5 +1,5 @@
 javascript: console.log(
   Array.from(
-    document.querySelectorAll('[class^="card-wrapper"] [class^="text"]')
+    document.querySelectorAll('[class^="card-wrapper"] [class^="content"]')
   ).map((x) => x.innerText.trim())
 );

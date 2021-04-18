1841c883c313990081c96b32a6c452307fef5cc9
hchiam <hchiam@users.noreply.github.com>
2021-04-18 18:53:46 -0400

    chrome file system access

--- read-json-file.js
+++ read-json-file.js
@@ -1,7 +1,36 @@
-// in the browser:
+// // in the browser:
 
-fetch("data.json")
-  .then((response) => response.json())
-  .then((jsonResponse) => console.log(jsonResponse));
+// fetch("data.json")
+//   .then((response) => response.json())
+//   .then((jsonResponse) => console.log(jsonResponse));
 
-// in console terminal CLI: https://github.com/hchiam/learning-nodejs/blob/master/README.md#read-a-local-file-via-console-cli
+// // in console terminal CLI: https://github.com/hchiam/learning-nodejs/blob/master/README.md#read-a-local-file-via-console-cli
+
+// (works in Chrome)
+// more: https://web.dev/file-system-access/
+
+const openButton = document.getElementById("open");
+
+let fileHandle;
+openButton.addEventListener("click", getFileContents);
+// openButton.addEventListener("click", getFolder);
+
+async function getFileContents() {
+  [fileHandle] = await window.showOpenFilePicker();
+  const file = await fileHandle.getFile();
+  const contents = await file.text();
+  console.log(contents);
+}
+
+async function getFolder() {
+  const dirHandle = await window.showDirectoryPicker();
+  // for await (const entry of dirHandle.values()) {
+  //   console.log(entry.kind, entry.name);
+  // }
+  const newFileHandle = await dirHandle.getFileHandle("test.json", {
+    create: false,
+  });
+  const file = await newFileHandle.getFile();
+  const contents = await file.text();
+  console.log(contents);
+}
--- read-text-file.js
+++ read-text-file.js
@@ -1,7 +1,36 @@
-// in the browser:
+// // in the browser:
 
-fetch("file.txt")
-  .then((response) => response.text())
-  .then((text) => console.log(text));
+// fetch("file.txt")
+//   .then((response) => response.text())
+//   .then((text) => console.log(text));
 
-// in console terminal CLI: https://github.com/hchiam/learning-nodejs/blob/master/README.md#read-a-local-file-via-console-cli
+// // in console terminal CLI: https://github.com/hchiam/learning-nodejs/blob/master/README.md#read-a-local-file-via-console-cli
+
+// (works in Chrome)
+// more: https://web.dev/file-system-access/
+
+const openButton = document.getElementById("open");
+
+let fileHandle;
+openButton.addEventListener("click", getFileContents);
+// openButton.addEventListener("click", getFolder);
+
+async function getFileContents() {
+  [fileHandle] = await window.showOpenFilePicker();
+  const file = await fileHandle.getFile();
+  const contents = await file.text();
+  console.log(contents);
+}
+
+async function getFolder() {
+  const dirHandle = await window.showDirectoryPicker();
+  // for await (const entry of dirHandle.values()) {
+  //   console.log(entry.kind, entry.name);
+  // }
+  const newFileHandle = await dirHandle.getFileHandle("test.txt", {
+    create: false,
+  });
+  const file = await newFileHandle.getFile();
+  const contents = await file.text();
+  console.log(contents);
+}

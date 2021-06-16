0478b614524a3e7213a3ad3c58551e7ca52a928f
hchiam <hchiam@users.noreply.github.com>
2021-06-15 23:32:08 -0400

    provide another perspective on the 2nd solution

--- more-like-interview-questions/insertDeleteGetRandomO1.js
+++ more-like-interview-questions/insertDeleteGetRandomO1.js
@@ -12,17 +12,20 @@
  *
  * The array helps, because you might as well store indices for the values in the hash table (key:value, value=?),
  * so store array indices in the hash table that match up with the array.
+ * The indices in the hash table will help to find in O(1) time where the item to remove is in the array.
+ *
  * And then the only surprising logic is when the item you're removing
  * just happens to not be the last one in the array (as detected by comparing with the array's last element),
  * in which case a "gap" is created in the array where you removed an item,
  * which breaks the usefulness of the array,
  * so to fix that, you can "fill" that gap in the array with the last item in the array,
+ * and make use of the index stored in the hash table for the index of the array that is now a "gap",
  * and update the hash table (which maps values to the array's indices) to match the updated array.
  *
  * For example:
  *
  * {0:0, 1:1, 2:2, 3:3} [0,1,2,3] remove 1
- * {0:0, 2:2, 3:3} [0,1,2,_] (need to update/correct the array)
+ * {0:0, 2:2, 3:3} [0,1,2,_] (need to update/correct the array, to remove 1)
  * indexOfRemoved = 1, lastValueAdded = 3
  * {0:0, 2:2, 3:1} [0,3,2] (fix the array, update hash table to match array)
  */
@@ -113,7 +116,7 @@ RandomizedSet2.prototype.remove = function (val) {
        * For example:
        *
        * {0:0, 1:1, 2:2, 3:3} [0,1,2,3] remove 1
-       * {0:0, 2:2, 3:3} [0,1,2,_] (need to update/correct the array)
+       * {0:0, 2:2, 3:3} [0,1,2,_] (need to update/correct the array, to remove 1)
        * indexOfRemoved = 1, lastValueAdded = 3
        * {0:0, 2:2, 3:1} [0,3,2] (fix the array, update hash table to match array)
        */

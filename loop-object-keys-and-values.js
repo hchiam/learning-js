72453a7e58ed08fc5a8b6c5ecf0eccb53189e6d2
hchiam <hchiam@users.noreply.github.com>
2021-06-17 21:43:40 -0400

    sorted descending

--- loop-object-keys-and-values.js
+++ loop-object-keys-and-values.js
@@ -2,8 +2,19 @@ const obj = {
   a: "Some string.",
   b: 42,
   1: { calvinAndHobbes: "transmogrifier" },
+  c: 3,
+  d: 4,
+  e: 0,
+  f: 1,
+  g: 9,
 };
 
 Object.entries(obj).forEach(([k, v]) => {
   console.log(k, v);
 });
+
+const sortedDesc = Object.entries(obj).sort((a, b) => {
+  return b[1] - a[1];
+});
+
+console.log("sorted descending:", sortedDesc);

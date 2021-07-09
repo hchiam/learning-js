0965bf14cea6557af3ea3fce9845ea5bb31f971a
hchiam <hchiam@users.noreply.github.com>
2021-07-08 20:35:47 -0400

    comment

--- more-like-interview-questions/dp-table-max-len-subarray.js
+++ more-like-interview-questions/dp-table-max-len-subarray.js
@@ -9,6 +9,8 @@ function findLength(nums1, nums2) {
   const dp = initializeDPTable(nums1, nums2);
   for (let i = 0; i < nums1.length; i++) {
     for (let j = 0; j < nums2.length; j++) {
+      // recurrence relation: if same, then start (1) or continue (diagonal+1)
+      // (ignore top, ignore left)
       if (nums1[i] === nums2[j]) {
         dp[i + 1][j + 1] = Math.max(1, 1 + dp[i][j]);
       }

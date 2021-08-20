0aa7b6d12cb124bdebe6e1d9cb007313a2e9fa02
hchiam <hchiam@users.noreply.github.com>
2021-08-20 16:28:00 -0400

    more thorough tests

--- randomInteger.test.js
+++ randomInteger.test.js
@@ -123,6 +123,34 @@ describe("getRandomInteger", () => {
     expect(output).toBeLessThan(10);
     expect(output).toBeGreaterThan(4);
   });
+
+  it("can hit both 0 and 1 in [0,2)", () => {
+    const a = 0;
+    const b = 2;
+    const output = [];
+    for (let i = 0; i < 100; i++) {
+      output.push(getRandomInteger(a, b));
+    }
+    const uniques = new Set();
+    for (let i = 0; i < 100; i++) {
+      uniques.add(output[i]);
+    }
+    expect(uniques.size).toBe(2);
+  });
+
+  it("can hit both 1 and 2 in [1,3)", () => {
+    const a = 1;
+    const b = 3;
+    const output = [];
+    for (let i = 0; i < 100; i++) {
+      output.push(getRandomInteger(a, b));
+    }
+    const uniques = new Set();
+    for (let i = 0; i < 100; i++) {
+      uniques.add(output[i]);
+    }
+    expect(uniques.size).toBe(2);
+  });
 });
 
 describe("getRandomInteger_Inclusive", () => {
@@ -249,5 +277,33 @@ describe("getRandomInteger_Inclusive", () => {
     expect(output % 1).toBe(0);
     expect(output).toBeLessThan(11);
     expect(output).toBeGreaterThan(4);
+  });
+
+  it("can hit both 0 and 1 in [0,1]", () => {
+    const a = 0;
+    const b = 1;
+    const output = [];
+    for (let i = 0; i < 100; i++) {
+      output.push(getRandomInteger_Inclusive(a, b));
+    }
+    const uniques = new Set();
+    for (let i = 0; i < 100; i++) {
+      uniques.add(output[i]);
+    }
+    expect(uniques.size).toBe(2);
+  });
+
+  it("can hit both 1 and 2 in [1,2]", () => {
+    const a = 1;
+    const b = 2;
+    const output = [];
+    for (let i = 0; i < 100; i++) {
+      output.push(getRandomInteger_Inclusive(a, b));
+    }
+    const uniques = new Set();
+    for (let i = 0; i < 100; i++) {
+      uniques.add(output[i]);
+    }
+    expect(uniques.size).toBe(2);
   });
 });

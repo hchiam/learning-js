1554baef3f77b1f3e660901f46134d42da269a18
hchiam <hchiam@users.noreply.github.com>
2021-09-07 17:00:48 -0400

    you can use [], but account for some intermediate cells having NaN

--- more-like-interview-questions/coinChange_notCareOrder.js
+++ more-like-interview-questions/coinChange_notCareOrder.js
@@ -1,23 +1,23 @@
 // FIXED with respect to coinChange_careOrder.js:
 // - SWAP THE 2 FOR LOOPS --> why? coin order doesn't matter!
-// - set up DP table with new Array and .fill
+// - DP table = new Array and .fill because = [] causes NaN+1 = NaN (because not initialized to 0)
 
 // 1+5 = 5+1:
 // 11111, 15(=51)
 console.log(coinChange_notCareOrder(6, [1, 5]) === 2);
 
+// 223, 34, 7
+console.log(coinChange_notCareOrder(7, [2, 3, 4, 7]) === 3);
+
 function coinChange_notCareOrder(target, denoms) {
-  const dp = new Array(target + 1).fill(0);
+  const dp = []; // new Array(n + 1).fill(0);
   dp[0] = 1; // there's one way to make 0, and 1 way to make 1
   // making coins the outer loop makes it not care about coin order
   for (let coin of denoms) {
     for (let i = 1; i <= target; i++) {
       if (i - coin >= 0) {
-        if (dp[i]) {
-          dp[i] += dp[i - coin];
-        } else {
-          dp[i] = dp[i - coin];
-        }
+        // dp[i] += dp[i - coin];
+        dp[i] = (dp[i] || 0) + (dp[i - coin] || 0);
       }
     }
   }

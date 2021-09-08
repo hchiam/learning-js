36cddc07b5163a8528dd0e71ed49a22c8ee5c6b2
hchiam <hchiam@users.noreply.github.com>
2021-09-08 17:49:44 -0400

    comments to make it easier to remember why 1 << numberOfBits and why i < numberOfBits

--- generateSubsets.js
+++ generateSubsets.js
@@ -1,14 +1,20 @@
 console.log(generateSubsets([1, 2, 3]));
 
+// Why until bitArray < (1 << numberOfItems) ?
+// Because we want to iterate numbers to generate all subsets with numberOfItems items.
+
+// Why until i < numberOfItems ?
+// Because we want to iterate that many bits = check which items are in the subset.
+
 function generateSubsets(array) {
   const subsets = [];
 
   // increment a bit array, to represent all possible combinations
-  const numberOfBits = array.length;
-  for (let bitArray = 0; bitArray < 1 << numberOfBits; bitArray++) {
+  const numberOfItems = array.length;
+  for (let bitArray = 0; bitArray < 1 << numberOfItems; bitArray++) {
     // 1 << 4 = 10000 -> 0000 to 1111
     const subset = [];
-    for (let i = 0; i < numberOfBits; i++) {
+    for (let i = 0; i < numberOfItems; i++) {
       // (1 << i) = check if bit used in bitArray
       if (bitArray & (1 << i)) subset.push(array[i]);
     }
@@ -21,7 +27,7 @@ function generateSubsets(array) {
 /**
  * example:
  *
- * numberOfBits = 3
+ * numberOfItems = 3
  * 1 << 3 = 1000
  * bitArray = 000 to 111 (0 to 7): 000,001,010,011,100,101,110,111 to generate subsets
  * i = 0 to 2 = shift 0 to 2 bits

f0a0c459bf6d830ece0dfc674ef22c725336b5d3
hchiam <hchiam@users.noreply.github.com>
2021-09-08 17:46:40 -0400

    comments to make it easier to remember why 1 << numberOfBits and why i < numberOfBits

--- generateSubsets.js
+++ generateSubsets.js
@@ -4,13 +4,26 @@ function generateSubsets(array) {
   const subsets = [];
 
   // increment a bit array, to represent all possible combinations
-  for (let b = 0; b < 1 << array.length; b++) {
+  const numberOfBits = array.length;
+  for (let bitArray = 0; bitArray < 1 << numberOfBits; bitArray++) {
+    // 1 << 4 = 10000 -> 0000 to 1111
     const subset = [];
-    for (let i = 0; i < array.length; i++) {
-      if (b & (1 << i)) subset.push(array[i]);
+    for (let i = 0; i < numberOfBits; i++) {
+      // (1 << i) = check if bit used in bitArray
+      if (bitArray & (1 << i)) subset.push(array[i]);
     }
     subsets.push(subset);
   }
 
   return subsets;
 }
+
+/**
+ * example:
+ *
+ * numberOfBits = 3
+ * 1 << 3 = 1000
+ * bitArray = 000 to 111 (0 to 7): 000,001,010,011,100,101,110,111 to generate subsets
+ * i = 0 to 2 = shift 0 to 2 bits
+ * (1 << i) = 001 to 100: 001,010,100 to check which bit used
+ */

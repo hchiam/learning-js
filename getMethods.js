b0f7ee2e8ecc7d569f225ab4663e269a0a513427
hchiam <hchiam@users.noreply.github.com>
2021-09-10 10:05:47 -0400

    could be used for code reflection / AOP

--- getMethods.js
+++ getMethods.js
@@ -1,5 +1,12 @@
 // nodemon -x 'node getMethods.js'
 
+/**
+ * Gets the methods of a JS object.
+ * This could then be used to replace methods.
+ * Which in turn could be used for code reflection
+ * and Aspect-Oriented Programming (AOP): https://blog.bitsrc.io/aspect-oriented-programming-in-javascript-c4cb43f6bfcc
+ */
+
 function getMethods(object, getImmediateOnly) {
   getImmediateOnly = getImmediateOnly || false;
 

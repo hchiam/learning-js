dd76ff0743067a2c99703a6f75397fd03c50d47a
hchiam <hchiam@users.noreply.github.com>
2021-09-10 13:31:14 -0400

    minLevenshteinDistance_better

--- minLevDistance.test.js
+++ minLevDistance.test.js
@@ -1,8 +1,11 @@
 /* eslint-disable require-jsdoc */
 
 const { solutionWrapper } = require("./minLevDistance.js");
+const {
+  minLevenshteinDistance_better: better,
+} = require("./minLevenshteinDistance_better.js");
 
-describe("the solution", () => {
+describe("the older solution", () => {
   it("works", () => expect(solutionWrapper("abc", "abd")).toStrictEqual(1));
   it("works", () => expect(solutionWrapper("abcd", "abd")).toStrictEqual(1));
   it("works", () => expect(solutionWrapper("abcde", "abd")).toStrictEqual(2));
@@ -11,4 +14,15 @@ describe("the solution", () => {
   it("works", () => expect(solutionWrapper("a", "a")).toStrictEqual(0));
   it("works", () => expect(solutionWrapper("b", "a")).toStrictEqual(1));
   it("works", () => expect(solutionWrapper("abc", "abc")).toStrictEqual(0));
+});
+
+describe("the better solution", () => {
+  it("works", () => expect(better("abc", "abd")).toStrictEqual(1));
+  it("works", () => expect(better("abcd", "abd")).toStrictEqual(1));
+  it("works", () => expect(better("abcde", "abd")).toStrictEqual(2));
+  it("works", () => expect(better("abc", "def")).toStrictEqual(3));
+  it("works", () => expect(better("a", "")).toStrictEqual(1));
+  it("works", () => expect(better("a", "a")).toStrictEqual(0));
+  it("works", () => expect(better("b", "a")).toStrictEqual(1));
+  it("works", () => expect(better("abc", "abc")).toStrictEqual(0));
 });
---
+++ minLevenshteinDistance_better.js
@@ -0,0 +1,42 @@
+// nodemon -x 'node minLevenshteinDistance_better.js'
+console.log(minLevenshteinDistance_better("abc", "yabd"));
+
+/*
+DP table:
+row = substring of the 2nd string
+1st row = empty string
+column = substring of the 1st string
+1st column = empty string
+		''		y		ya		yab		yabd
+''	0 		1		2 		3  		4
+a		1 		1		1 		2  		3
+b		2 		2		2 		1  		2
+c		3 		3		3 		2  		2 = add y, swap c with d
+*/
+
+function minLevenshteinDistance_better(str1, str2) {
+  const dp = new Array(str1.length + 1)
+    .fill(null)
+    .map(() => new Array(str2.length + 1).fill(Infinity));
+  for (let i = 0; i < str1.length + 1; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 0; j < str2.length + 1; j++) {
+    dp[0][j] = j;
+  }
+  for (let i = 1; i < str1.length + 1; i++) {
+    for (let j = 1; j < str2.length + 1; j++) {
+      const swap = (str1[i - 1] === str2[j - 1] ? 0 : 1) + dp[i - 1][j - 1];
+      const insertB = dp[i][j - 1] + 1;
+      const removeA = dp[i - 1][j] + 1;
+      dp[i][j] = Math.min(swap, insertB, removeA);
+    }
+  }
+  const lastCell = dp.slice(-1)[0].slice(-1)[0];
+  return lastCell;
+}
+
+// Do not edit the line below.
+module.exports = {
+  minLevenshteinDistance_better,
+};

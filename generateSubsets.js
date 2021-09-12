f83a612013cd9e6752c884886c73db895492dd0a
hchiam <hchiam@users.noreply.github.com>
2021-09-12 19:55:52 -0400

    alternative solution, time/space complexity, and nodemon command ready for use

--- generateSubsets.js
+++ generateSubsets.js
@@ -1,22 +1,29 @@
-console.log(generateSubsets([1, 2, 3]));
+// nodemon -x 'node generateSubsets.js'
 
-// Why until bitArray < (1 << numberOfItems) ?
+console.log(
+  generateSubsets_binaryMethod([1, 2, 3]).sort(
+    (a, b) => Number(a.join("")) - Number(b.join(""))
+  )
+);
+
+// Why until binaryCombo < (1 << numberOfItems) ?
 // Because we want to iterate numbers to generate all subsets with numberOfItems items.
 
 // Why until i < numberOfItems ?
 // Because we want to iterate that many bits = check which items are in the subset.
 
-function generateSubsets(array) {
+// O(n * 2^n) time, O(n * 2^n) space
+function generateSubsets_binaryMethod(array) {
   const subsets = [];
 
   // increment a bit array, to represent all possible combinations
   const numberOfItems = array.length;
-  for (let bitArray = 0; bitArray < 1 << numberOfItems; bitArray++) {
+  for (let binaryCombo = 0; binaryCombo < 1 << numberOfItems; binaryCombo++) {
     // 1 << 4 = 10000 -> 0000 to 1111
     const subset = [];
     for (let i = 0; i < numberOfItems; i++) {
-      // (1 << i) = check if bit used in bitArray
-      if (bitArray & (1 << i)) subset.push(array[i]);
+      // (1 << i) = check if bit used in binaryCombo
+      if (binaryCombo & (1 << i)) subset.push(array[i]);
     }
     subsets.push(subset);
   }
@@ -29,7 +36,37 @@ function generateSubsets(array) {
  *
  * numberOfItems = 3
  * 1 << 3 = 1000
- * bitArray = 000 to 111 (0 to 7): 000,001,010,011,100,101,110,111 to generate subsets
+ * binaryCombo = 000 to 111 (0 to 7): 000,001,010,011,100,101,110,111 to generate subsets
  * i = 0 to 2 = shift 0 to 2 bits
  * (1 << i) = 001 to 100: 001,010,100 to check which bit used
  */
+
+/**
+ * alternatively:
+ *
+ * [] // <-- append 1 to this existing set
+ * [], [1] // <-- append 2 to these existing sets
+ * [], [1], [2], [1, 2] // <-- append 3 to these existing sets
+ * [], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3] // same result as other way!
+ */
+
+console.log(
+  generateSubsets_appendToExistingWay([1, 2, 3]).sort(
+    (a, b) => Number(a.join("")) - Number(b.join(""))
+  )
+);
+
+// O(n * 2^n) time, O(n * 2^n) space
+function generateSubsets_appendToExistingWay(array) {
+  const subsets = [[]]; // initialize with [] set
+
+  for (let item of array) {
+    const lengthBeforePushAnother = subsets.length;
+    for (let i = 0; i < lengthBeforePushAnother; i++) {
+      const subset = subsets[i];
+      subsets.push([...subset, item]);
+    }
+  }
+
+  return subsets;
+}

757885889450fa0b722cf755e36e0fb28532ce7d
hchiam <hchiam@users.noreply.github.com>
2021-10-12 18:10:20 -0400

    extra tweaks

--- more-like-interview-questions/minJumps.js
+++ more-like-interview-questions/minJumps.js
@@ -1,4 +1,10 @@
-// Ot(n), Os(1)
+/**
+ * Ot(n), Os(1) = BCR!
+ * Key insight for most optimal (BCR) solution:
+ * 		Update the next max reach, while checking whether you need to jump yet.
+ * 		i.e. current max reach = previous max reach where you only add +1 jump,
+ * 		and get next range by updating max reach from all in the current range.
+ */
 function minJumps(array) {
   if (!array || array.length < 2) return 0;
 
@@ -7,7 +13,9 @@ function minJumps(array) {
   let currentReach = 0; // MUST immediately jump from start
   let nextReach = 0; // will update as we go
 
-  for (let i = 0; i < array.length - 1; i++) {
+  // for (let i = 0; i < array.length - 1; i++) {
+  for (let i in array) {
+    i = Number(i);
     const jumpSize = array[i];
     nextReach = Math.max(nextReach, i + jumpSize);
 
@@ -15,8 +23,8 @@ function minJumps(array) {
     if (hitEndOfCurrentReach) {
       currentReach = nextReach; // update current reach
       jumps++; // jump into current reach
-
-      const jumpedIntoReachContainingEnd = currentReach >= array.length - 1;
+      const lastIndex = array.length - 1;
+      const jumpedIntoReachContainingEnd = currentReach >= lastIndex;
       if (jumpedIntoReachContainingEnd) {
         return jumps;
       }

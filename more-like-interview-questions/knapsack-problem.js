ea43ba3d81b7cd6733bae113c8b2601ab39979d3
hchiam <hchiam@users.noreply.github.com>
2021-10-12 23:46:12 -0400

    better console.log output

--- more-like-interview-questions/knapsack-problem.js
+++ more-like-interview-questions/knapsack-problem.js
@@ -1,15 +1,16 @@
+const testOutput = knapsackProblem(
+  [
+    [1, 2],
+    [4, 3],
+    [5, 6],
+    [6, 7],
+  ],
+  10
+);
+
 console.log(
-  JSON.stringify(
-    knapsackProblem(
-      [
-        [1, 2],
-        [4, 3],
-        [5, 6],
-        [6, 7],
-      ],
-      10
-    )
-  ) === JSON.stringify([10, [3, 1]])
+  JSON.stringify(testOutput) === JSON.stringify([10, [3, 1]]),
+  testOutput
 );
 
 /*

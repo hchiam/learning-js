a489d219184b209a26e1db8d2e44f02cab0b86be
hchiam <hchiam@users.noreply.github.com>
2021-10-13 21:47:58 -0400

    cleaner

--- more-like-interview-questions/numbersInPi.js
+++ more-like-interview-questions/numbersInPi.js
@@ -10,7 +10,7 @@ const numbers = [
 ];
 
 const output = numbersInPi(pi, numbers);
-console.log(output, output === 2);
+console.log(pi, "\n", numbers, "\nanswer:", output, "\n", output === 2);
 
 /*
 n = digits of pi given, m = numbers given
@@ -34,68 +34,26 @@ observations:
 function numbersInPi(pi, numbers) {
   let numberOfSpaces = Infinity;
   const memo = {};
-  // locationsPerNumber: {#:[,]} range inclusive i to exclusive i for easier slicing
-  // const locationsPerNumber = getLocationsPerNumber(pi, numbers);
-  // numberOfSpaces = recursivelyCheckPrefixes(locationsPerNumber, pi, 0, memo);
   for (let number of numbers) {
     memo[number] = 0;
   }
   numberOfSpaces = recursivelyCheckPrefixes(memo, pi, 0, memo);
-  console.log(memo);
+  console.log("\nmemo:", memo, "\n");
   return numberOfSpaces === Infinity ? -1 : numberOfSpaces;
 }
-
-// function getLocationsPerNumber(pi, numbers) {
-//   const locationsPerNumber = {};
-//   for (let n = 0; n < numbers.length; n++) {
-//     const number = numbers[n];
-//     const earlyEnd = number.length - 1;
-//     for (let p = 0; p < pi.length - earlyEnd; p++) {
-//       const sectionOfPi = pi.slice(p, p + number.length);
-//       if (sectionOfPi === number) {
-//         if (number in locationsPerNumber) {
-//           // exclusive range p to p + number.length for easier slicing:
-//           locationsPerNumber[number].push([p, p + number.length]);
-//         } else {
-//           locationsPerNumber[number] = [p, p + number.length];
-//         }
-//       }
-//     }
-//   }
-//   return locationsPerNumber;
-// }
-
-// locationsPerNumber: {number:[inclusive,exclusive]}
-// function recursivelyCheckPrefixes(locsPerNum, pi, spaces = 0, memo) {
-function recursivelyCheckPrefixes(memo, pi, spaces = 0) {
-  if (pi in memo /*locsPerNum*/) {
-    // base case
-    // memo[pi] = pi in memo ? Math.min(memo[pi], spaces) : spaces;
-    // memo[pi] = 0; // just this?
+function recursivelyCheckPrefixes(memo, pi) {
+  if (pi in memo) {
     return memo[pi];
   }
   for (let p = 0; p < pi.length; p++) {
     for (let q = p + 1; q < pi.length; q++) {
       const prefix = pi.slice(p, q);
       const suffix = pi.slice(q, pi.length);
-      if (prefix in memo /*locsPerNum*/) {
-        // memo[prefix] = 0;
-        const s = recursivelyCheckPrefixes(memo /*locsPerNum*/, suffix, 0);
+      if (prefix in memo) {
+        const s = recursivelyCheckPrefixes(memo, suffix);
         const pPlusS = memo[prefix] + 1 + s;
-        console.log(
-          "\n",
-          "\n",
-          memo[prefix],
-          1,
-          s,
-          "=",
-          pPlusS,
-          prefix,
-          suffix,
-          pi
-        );
         const ps = prefix + suffix;
-        memo[ps] = ps in memo ? Math.min(memo[ps], pPlusS) : pPlusS; //pi in memo ? Math.min(memo[pi], pPlusS) : pPlusS;
+        memo[ps] = ps in memo ? Math.min(memo[ps], pPlusS) : pPlusS;
       }
     }
   }

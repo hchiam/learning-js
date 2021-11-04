83ec8315f9786800292373aee3e4cfff79a89bec
hchiam <hchiam@users.noreply.github.com>
2021-11-03 23:10:12 -0400

    animate

--- bookmarklets/highlightMemriseToReview.js
+++ bookmarklets/highlightMemriseToReview.js
@@ -1,11 +1,42 @@
 javascript: (function () {
+  document.head.querySelector("#highlight_memrise_to_review")?.remove();
   const toReview = Array.from(
     document.querySelectorAll(
       '[title*="Review"],[data-original-title*="Review"]'
     )
   );
   toReview.map((x) => {
-    x.style.background = "red";
+    x.style.setProperty("--original-top", x.getBoundingClientRect().top + "px");
+    x.style.setProperty(
+      "--original-left",
+      x.getBoundingClientRect().left + "px"
+    );
   });
-  alert(`${toReview.length} to review! :)`);
+  document.head.insertAdjacentHTML(
+    "beforeend",
+    `<style id="highlight_memrise_to_review">
+      [title*="Review"],[data-original-title*="Review"] {
+        animation: move 2s;
+        position: fixed;
+        transition: top 0.2s;
+        left: var(--original-left);
+        background: red;
+        z-index: 1000;
+        border-radius: 5px;
+      }
+      
+      @keyframes move {
+        0% {
+          top: 0;
+        }
+        100% {
+          top: var(--original-top);
+        }
+      }
+    </style>`
+  );
+
+  setTimeout(() => {
+    alert(`${toReview.length} to review! :)`);
+  }, 2000);
 })();

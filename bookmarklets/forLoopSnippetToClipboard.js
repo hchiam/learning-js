05dd3e8d2766ff6a896b02661ae058f9c5ce9afd
hchiam <hchiam@users.noreply.github.com>
2021-11-04 22:28:47 -0400

    , -> ; for easier reading

--- bookmarklets/forLoopSnippetToClipboard.js
+++ bookmarklets/forLoopSnippetToClipboard.js
@@ -1,11 +1,11 @@
 javascript: (function () {
-  var textarea = document.createElement("textarea"),
-    selection = document.getSelection();
-  (textarea.textContent = "for (let i = 0; i < array.length; i++)"),
-    document.body.appendChild(textarea),
-    selection.removeAllRanges(),
-    textarea.select(),
-    document.execCommand("copy"),
-    selection.removeAllRanges(),
-    document.body.removeChild(textarea);
+  var textarea = document.createElement("textarea");
+  selection = document.getSelection();
+  textarea.textContent = "for (let i = 0; i < array.length; i++)";
+  document.body.appendChild(textarea);
+  selection.removeAllRanges();
+  textarea.select();
+  document.execCommand("copy");
+  selection.removeAllRanges();
+  document.body.removeChild(textarea);
 })();

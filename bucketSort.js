d1c5bd2dd8e5b6281c5373973c2689eba146a329
hchiam <hchiam@users.noreply.github.com>
2021-11-16 12:16:59 -0500

    notes

--- bucketSort.js
+++ bucketSort.js
@@ -1,5 +1,11 @@
 /**
- * Ot(n log b)
+ * Ot(n b log b)
+ *
+ * But apparently it's more like Ot(n log n) worst case
+ * and Ot(n + n^2 / b + b) on average,
+ * or Ot(n) on average when b ~ n.
+ * https://en.wikipedia.org/wiki/Bucket_sort
+ * (Average case if uniform distribution.)
  */
 function bucketSort(array, approximateBucketSize = 1) {
   const min = Math.min(...array);
@@ -9,17 +15,20 @@ function bucketSort(array, approximateBucketSize = 1) {
   const fencePostCount = range + 1;
 
   const buckets2DArray = Array.from({ length: fencePostCount }, () => []);
+
+  // Ot(n)
   array.forEach((item) => {
-    // Ot(n)
     const bucketIndex = Math.floor((item - min) / (approximateBucketSize || 1));
     buckets2DArray[bucketIndex].push(item);
   });
 
   const output = [];
+
+  // Ot(n)
   buckets2DArray.forEach((bucket) => {
-    // Ot(n)
-    output.push(...bucket.sort((a, b) => a - b)); // Ot(b)
+    output.push(...bucket.sort((a, b) => a - b)); // Ot(b log b)
   });
+
   // console.log(buckets2DArray);
   return output;
 }

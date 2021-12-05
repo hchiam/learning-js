0237b5dd571c000e965c0f7006761a7b600e4515
hchiam <hchiam@users.noreply.github.com>
2021-12-05 12:11:22 -0500

    xpath_get_elements_by_text.js

---
+++ xpath_get_elements_by_text.js
@@ -0,0 +1,34 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/evaluate
+
+// EXAMPLE:
+// var xpath = "//p[contains(text(),'Some text contained in a p tag')]";
+var xpath =
+  "//*[contains(@class, 'kix-appview-editor-container')]//*[contains(text(),'some text')]";
+var elements = getElementsByText(xpath);
+console.log(elements);
+
+function getElementsByText(xpath) {
+  var iterator = document.evaluate(
+    xpath,
+    document,
+    null,
+    XPathResult.ORDERED_NODE_ITERATOR_TYPE,
+    null
+  );
+
+  var elements = [];
+  var element = null;
+  while ((element = iterator.iterateNext())) {
+    elements.push(element);
+  }
+
+  return elements;
+}
+
+function getElementsByTextInGoogleDocInFirefox(text) {
+  var xpath =
+    "//*[contains(@class, 'kix-appview-editor-container')]//*[contains(@class, 'kix-lineview') and contains(.//span, '" +
+    text +
+    "')]"; // "/parent::span/parent::div/parent::div[contains(@class, 'kix-lineview')]";
+  return getElementsByText(xpath);
+}

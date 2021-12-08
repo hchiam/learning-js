66dcd13fd35d03f88e96ea464e02c2aae1798253
hchiam <hchiam@users.noreply.github.com>
2021-12-07 22:52:14 -0500

    yarn lint

--- fetch-github-latest-release-name.js
+++ fetch-github-latest-release-name.js
@@ -1,2 +1,4 @@
 // example:
-fetch('https://api.github.com/repos/hchiam/html-template-generator/releases').then(r=>r.json()).then(r=>console.log(r[0].name));
+fetch("https://api.github.com/repos/hchiam/html-template-generator/releases")
+  .then((r) => r.json())
+  .then((r) => console.log(r[0].name));
--- trie.js
+++ trie.js
@@ -1,48 +1,48 @@
-var TrieNode = function(value, isEndOfWord = false) {
+var TrieNode = function (value, isEndOfWord = false) {
   this.value = value;
   this.children = {};
   this.isEndOfWord = isEndOfWord;
-}
-
-var Trie = function() {
-  const isEndOfWord = true;
-  this.root = new TrieNode('', isEndOfWord);
 };
 
-Trie.prototype.insert = function(word) {
+var Trie = function () {
+  const isEndOfWord = true;
+  this.root = new TrieNode("", isEndOfWord);
+};
+
+Trie.prototype.insert = function (word) {
   let pointer = this.root;
   for (let c of word) {
-      if (!(c in pointer.children)) {
-          pointer.children[c] = new TrieNode(c);
-      }
-      pointer = pointer.children[c];
+    if (!(c in pointer.children)) {
+      pointer.children[c] = new TrieNode(c);
+    }
+    pointer = pointer.children[c];
   }
   pointer.isEndOfWord = true;
 };
 
-Trie.prototype.search = function(word) {
+Trie.prototype.search = function (word) {
   let pointer = this.root;
   for (let c of word) {
-      if (!(c in pointer.children)) return false;
-      pointer = pointer.children[c];
+    if (!(c in pointer.children)) return false;
+    pointer = pointer.children[c];
   }
   return pointer.isEndOfWord;
 };
 
-Trie.prototype.startsWith = function(prefix) {
+Trie.prototype.startsWith = function (prefix) {
   let pointer = this.root;
   for (let c of prefix) {
-      if (!(c in pointer.children)) return false;
-      pointer = pointer.children[c];
+    if (!(c in pointer.children)) return false;
+    pointer = pointer.children[c];
   }
   return true;
 };
 
-/** 
-* Example usage:
-* 
-* var obj = new Trie();
-* obj.insert(word);
-* var hasWord = obj.search(word);
-* var hasPrefix = obj.startsWith(prefix);
-*/
+/**
+ * Example usage:
+ *
+ * var obj = new Trie();
+ * obj.insert(word);
+ * var hasWord = obj.search(word);
+ * var hasPrefix = obj.startsWith(prefix);
+ */

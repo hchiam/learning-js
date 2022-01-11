b41e3dc0f26016c591009d76c7d11d4f33e7b748
hchiam <hchiam@users.noreply.github.com>
2022-01-11 17:39:43 -0500

    navigator.clipboard.writeText if not IE

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -2,16 +2,20 @@
 
 function copyToClipboard(text) {
   try {
-    var temp = document.createElement("input");
-    document.body.append(temp);
-    temp.value = text;
-    temp.select();
-    document.execCommand("copy");
-    temp.remove();
-  } catch (err) {
-    alert(
-      "Could not automatically copy to clipboard. \n\n Copy this text instead: \n\n" +
-        text
-    );
+    navigator.clipboard.writeText(text); // if not IE
+  } catch (e) {
+    try {
+      var temp = document.createElement("input");
+      document.body.append(temp);
+      temp.value = text;
+      temp.select();
+      document.execCommand("copy");
+      temp.remove();
+    } catch (err) {
+      alert(
+        "Could not automatically copy to clipboard. \n\n Copy this text instead: \n\n" +
+          text
+      );
+    }
   }
 }

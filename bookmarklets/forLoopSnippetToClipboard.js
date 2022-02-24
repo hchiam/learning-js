86f074a4d326dec7ab1ffc77301acce57e1a0ce2
hchiam <hchiam@users.noreply.github.com>
2022-02-23 20:51:27 -0500

    better clipboard code

--- bookmarklets/forLoopSnippetToClipboard.js
+++ bookmarklets/forLoopSnippetToClipboard.js
@@ -1,11 +1,16 @@
 javascript: (function () {
-  var textarea = document.createElement("textarea");
-  selection = document.getSelection();
-  textarea.textContent = "for (let i = 0; i < array.length; i++)";
-  document.body.appendChild(textarea);
-  selection.removeAllRanges();
-  textarea.select();
-  document.execCommand("copy");
-  selection.removeAllRanges();
-  document.body.removeChild(textarea);
+  const toClipboard = "for (let i = 0; i < array.length; i++)";
+  copy(toClipboard);
+  function copy(text) {
+    var textarea = document.createElement("textarea");
+    selection = document.getSelection();
+    textarea.textContent = text;
+    document.body.appendChild(textarea);
+    selection.removeAllRanges();
+    textarea.select();
+    document.execCommand("copy");
+    selection.removeAllRanges();
+    document.body.removeChild(textarea);
+    alert(`Copied to clipboard:\n\n${text}`);
+  }
 })();

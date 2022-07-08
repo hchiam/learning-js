a658d31c2bded80755ba50e1164fc5ca9103abcb
hchiam <hchiam@users.noreply.github.com>
2022-07-08 17:38:06 -0600

    preorder, postorder, inorder traversal

---
+++ pre-order_post-order_in-order_traversal.js
@@ -0,0 +1,14 @@
+function pre(n, process) {
+  // pre-order = me, left, right:
+  if (n) process(n);
+  if (n.left) pre(n.left, process);
+  if (n.right) pre(n.right, process);
+} // post-order = left, right, me; in-order = left, me, right
+
+var n = { v: 1 };
+n.left = { v: 2 };
+n.right = { v: 3 };
+n.left.left = { v: 4 };
+n.left.right = { v: 5 };
+
+pre(n, (x) => console.log(x.v)); // 1, 2, 4, 5, 3

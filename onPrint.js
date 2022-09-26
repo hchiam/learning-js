831431d0e70d15ec32c66eaf2969409fb6edfa3f
hchiam <hchiam@users.noreply.github.com>
2022-09-25 21:05:26 -0600

    on print event listeners

---
+++ onPrint.js
@@ -0,0 +1,23 @@
+/** https://stackoverflow.com/a/15662720 */
+function onPrint(callback, backupEventName = "beforeprint") {
+  if ("matchMedia" in window) {
+    window.matchMedia("print").addListener(function (media) {
+      if (media.matches) {
+        callback();
+      } else {
+        // fires immediately, so wait for 1st mouse movement
+        $(document).one("mouseover", callback); // run just once
+      }
+    });
+  } else {
+    $(window).on(backupEventName, callback);
+  }
+}
+
+function onBeforePrint(callback) {
+  onPrint(callback, "beforeprint");
+}
+
+function onAfterPrint(callback) {
+  onPrint(callback, "afterprint");
+}

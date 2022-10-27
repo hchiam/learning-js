956eef2cbf4c3de42e1b41299e961d74bf765755
hchiam <hchiam@users.noreply.github.com>
2022-10-26 18:26:59 -0600

    onEnterViewport_intersectionObserver.js

---
+++ onEnterViewport_intersectionObserver.js
@@ -0,0 +1,15 @@
+function onEnterViewport(element, callback, options) {
+  if (!IntersectionObserver) return;
+
+  if (!options) options = { threshold: [0, 1] };
+  const observer = new IntersectionObserver(handleIntersect, options);
+  observer.observe(element);
+
+  function handleIntersect(entries, observer) {
+    entries.forEach((entry) => {
+      if (typeof callback === "function") {
+        callback(entry.intersectionRatio, observer);
+      }
+    });
+  }
+}

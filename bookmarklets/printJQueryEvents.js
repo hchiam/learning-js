3ad24e55956869df55b29789c295716261471284
hchiam <hchiam@users.noreply.github.com>
2022-12-16 16:29:59 -0700

    print out event handlers directly (as string and as clickable reference) too, since that is the most common thing i click to expand to see

--- bookmarklets/printJQueryEvents.js
+++ bookmarklets/printJQueryEvents.js
@@ -1,6 +1,27 @@
 javascript: (function () {
   var selector = prompt("jQuery selector?");
   $(selector).each(function () {
-    console.log($._data($(this).get(0), "events"));
+    var events = $._data($(this).get(0), "events");
+    if (!events) {
+      console.log(
+        "(no jQuery events found on %c" + selector + "%c)",
+        "color: lime; background: black;"
+      );
+      return;
+    }
+    var eventNames = Object.keys(events);
+    console.log(events);
+    eventNames.map(function (eventName) {
+      events[eventName].map(function (listener) {
+        var namespace = listener.namespace ? "." + listener.namespace : "";
+        console.groupCollapsed(
+          "%c" + eventName + namespace + "%c: \n↳ handler.toString()",
+          "color: lime; background: black;"
+        );
+        console.log(listener.handler.toString());
+        console.groupEnd();
+        console.log(listener.handler);
+      });
+    });
   });
 })();

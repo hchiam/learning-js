c2152711862dd9f7c5bbbce6a419094a69f36727
hchiam <hchiam@users.noreply.github.com>
2023-01-02 16:43:41 -0700

    get 2D array - as helper function

--- array-fill-map-2d-array.js
+++ array-fill-map-2d-array.js
@@ -1,14 +1,21 @@
 // mnemonic: afm afm
-var rows = 3;
-var cols = 5;
-var array2d = new Array(rows).fill(null).map(() =>
-  // afm
-  new Array(cols).fill(null).map(() => {
+function get2dArray(rows, cols, valueCallback) {
+  return new Array(rows).fill(null).map(() =>
     // afm
-    return { deep: "copy" };
-  })
-);
+    new Array(cols).fill(null).map(() => {
+      // afm
+      return valueCallback(); // or just put { deep: "copy" } here
+    })
+  );
+}
 
-array2d[rows - 1][cols - 1].deep = "edited just 1 NOT all";
+var rows = 2;
+var cols = 3;
+var valueCallback = () => {
+  return { deep: "copy" };
+};
+var array2d = get2dArray(rows, cols, valueCallback);
+
+array2d[rows - 1][cols - 1].deep = "edited 1 NOT all";
 
 console.log(array2d);

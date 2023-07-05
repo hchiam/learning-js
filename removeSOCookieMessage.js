560803405b53c69c574141e1c7867e358a683039
hchiam <hchiam@users.noreply.github.com>
2023-07-04 19:55:28 -0600

    prevent already-hidden banner from breaking any following appended JavaScript

--- removeSOCookieMessage.js
+++ removeSOCookieMessage.js
@@ -1,5 +1,5 @@
 (function () {
   if ("stackoverflow.com" === location.hostname) {
-    document.querySelector(".js-consent-banner").remove();
+    document.querySelector(".js-consent-banner")?.remove();
   }
 })();

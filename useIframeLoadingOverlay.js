36f81cdc37e31bc760c70aabe988934a35886824
hchiam <hchiam@users.noreply.github.com>
2023-07-17 16:14:31 -0600

    useIframeLoadingOverlay.js

---
+++ useIframeLoadingOverlay.js
@@ -0,0 +1,34 @@
+// written assuming jQuery is available:
+function useIframeLoadingOverlay(
+  allLoadedCallback,
+  selector = "iframe:visible"
+) {
+  const iframeLoadingOverlay = $(".iframe-loading-overlay");
+  const originalIframesLoading = $(selector);
+  const originalCountOfIframesLoading = originalIframesLoading.length;
+  if (originalCountOfIframesLoading === 0) {
+    iframeLoadingOverlay.addClass("d-none");
+  } else {
+    iframeLoadingOverlay.removeClass("d-none");
+    const progressBar = iframeLoadingOverlay.find("progress");
+    progressBar.val(0);
+    originalIframesLoading
+      .addClass("wait-for-iframe-to-load")
+      .one("load", function () {
+        $(this).removeClass("wait-for-iframe-to-load");
+        const recountCurrentIframesLoading = $(selector).filter(
+          ".wait-for-iframe-to-load"
+        ).length;
+        if (recountCurrentIframesLoading > 0) {
+          iframeLoadingOverlay.removeClass("d-none");
+          const completed =
+            originalCountOfIframesLoading - recountCurrentIframesLoading;
+          const percent = (completed / originalCountOfIframesLoading) * 100;
+          progressBar.val(percent);
+        } else {
+          iframeLoadingOverlay.addClass("d-none");
+          allLoadedCallback?.();
+        }
+      });
+  }
+}

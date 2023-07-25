d1d345379700bafe2a409d024af752e2a1e59616
hchiam <hchiam@users.noreply.github.com>
2023-07-24 23:14:34 -0600

    working demo

--- askBeforeLeaveOrCloseOrRefreshPage.js
+++ askBeforeLeaveOrCloseOrRefreshPage.js
@@ -1,11 +1,11 @@
 function askBeforeUnloadPage(shouldAskCallback) {
-  window.addEventListener("beforeunload", (e) => {
+  window.addEventListener("beforeunload", (event) => {
     // TODO: promises: https://dev.to/chromiumdev/sure-you-want-to-leavebrowser-beforeunload-event-4eg5
-    if (shouldAskCallback?.()) return;
+    if (shouldAskCallback && !shouldAskCallback?.(event)) return;
 
     // https://stackoverflow.com/a/19538231
     let confirmationMessage = "o/";
-    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    (event || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage; //Webkit, Safari, Chrome
   });
 }

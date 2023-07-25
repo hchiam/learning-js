5f8a44a51adc7e379a5220ae9a373a0a0bafa851
hchiam <hchiam@users.noreply.github.com>
2023-07-25 17:16:18 -0600

    event does not give original target, and "?" is unnecessary

--- askBeforeLeaveOrCloseOrRefreshPage.js
+++ askBeforeLeaveOrCloseOrRefreshPage.js
@@ -1,7 +1,7 @@
 function askBeforeUnloadPage(shouldAskCallback) {
   window.addEventListener("beforeunload", (event) => {
     // TODO: promises: https://dev.to/chromiumdev/sure-you-want-to-leavebrowser-beforeunload-event-4eg5
-    if (shouldAskCallback && !shouldAskCallback?.(event)) return;
+    if (shouldAskCallback && !shouldAskCallback()) return;
 
     // https://stackoverflow.com/a/19538231
     let confirmationMessage = "o/";

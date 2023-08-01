d01924b527a7c2eda0385fa1ec82e3ae3978bb9b
hchiam <hchiam@users.noreply.github.com>
2023-07-31 21:00:21 -0600

    custom reader/focus mode for Jira ticket page

---
+++ bookmarklets/jira-zen-custom-reader-focus-mode.js
@@ -0,0 +1,20 @@
+javascript: (() => {
+  if (document.body.classList.contains("zen")) {
+    document.body.classList.remove("zen");
+    document.body.removeAttribute("inert");
+  } else {
+    document.body.classList.add("zen");
+    document.body.setAttribute("inert", true);
+  }
+  if (!document.querySelector("style").innerText.includes(".zen {")) {
+    document.querySelector("style").innerText += `.zen {
+      --bannerHeight: 0;
+      --topNavigationHeight: 0;
+      --leftSidebarWidth: 0;
+      --rightSidebarWidth: 0;
+    }
+    .zen header, .zen [data-testid="issue.views.issue-details.issue-layout.container-right"] {
+      display: none !important;
+    }`;
+  }
+})();

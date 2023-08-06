df9766456b39b95c18ce26bd8b1d19e1a1b8400e
hchiam <hchiam@users.noreply.github.com>
2023-08-06 16:10:19 -0600

    solution and notes for House Robber II

--- more-like-interview-questions/houseRobber.test.js
+++ more-like-interview-questions/houseRobber.test.js
@@ -1,4 +1,5 @@
 const { rob1, rob2 } = require("./houseRobber.js");
+const { robii1, robii2 } = require("./houseRobberII.js");
 
 describe("Ot(n) Os(n) solution to the House Robber problem", () => {
   it("works", () => {
@@ -19,5 +20,33 @@ describe("Ot(n) Os(1) solution to the House Robber problem", () => {
     expect(rob2([1])).toBe(1);
     expect(rob2([1, 2])).toBe(2);
     expect(rob2([1, 2, 3])).toBe(4);
+  });
+});
+
+describe("Ot(n) Os(n) solution to the House Robber II problem", () => {
+  it("works", () => {
+    expect(robii1([1, 2, 3, 1])).toBe(4);
+    expect(robii1([2, 7, 9, 3, 1])).toBe(11);
+    expect(robii1([4, 1, 1, 4])).toBe(5);
+    expect(robii1([1])).toBe(1);
+    expect(robii1([1, 2])).toBe(2);
+    expect(robii1([1, 2, 3])).toBe(3);
+    expect(robii1([2, 3, 2])).toBe(3);
+    expect(robii1([0, 0, 0, 4])).toBe(4);
+    expect(robii1([4, 0, 0, 0])).toBe(4);
+  });
+});
+
+describe("Ot(n) Os(1) solution to the House Robber II problem", () => {
+  it("works", () => {
+    expect(robii2([1, 2, 3, 1])).toBe(4);
+    expect(robii2([2, 7, 9, 3, 1])).toBe(11);
+    expect(robii2([4, 1, 1, 4])).toBe(5);
+    expect(robii2([1])).toBe(1);
+    expect(robii2([1, 2])).toBe(2);
+    expect(robii2([1, 2, 3])).toBe(3);
+    expect(robii2([2, 3, 2])).toBe(3);
+    expect(robii2([0, 0, 0, 4])).toBe(4);
+    expect(robii2([4, 0, 0, 0])).toBe(4);
   });
 });
---
+++ more-like-interview-questions/houseRobberII.js
@@ -0,0 +1,107 @@
+// https://leetcode.com/problems/house-robber-ii/
+// https://github.com/hchiam/learning-js/blob/main/more-like-interview-questions/houseRobber.js
+
+/**
+ * key intuition: for circular array for house robber problem:
+ * just run solution twice: max of (yes first no last, no first yes last).
+ **/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+naive: all valid combos: scan 1-n then 1+1-n+1 etc Ot(n^2) Os(1)
+BCR: Ot(n) Os(1)
+dp table? Ot(n) Os(n) --> try this first
+1 var? Ot(n) Os(1) --> do this later if time
+remember: recursive relationship --> memo --> iterative --> variables
+BUT NOW CIRCULAR ARRAY! 
+    --> basically can't use both start and end indices
+    ideas:
+        maybe start at index 1 and ignore index 0 until loop back?
+            won't work with: 4411
+        maybe duplicate first at end and track whether used it?
+        or store a variable that indicates whether it was used? Os(n)?
+        or store a variable that indicates whether it was used? Os(1)?
+        or simply check if last index gave max? Os(1)?
+        but need to check both, so likely still Os(n), for now?
+            then Ot(n) Os(n) < BCR
+            try getting Os(1) later if time
+    is there any case where you have to loop +2 spaces? try examples:
+        010000004
+            nah?
+    is there any case where you have to loop +1 spaces? try examples:
+        010000040
+            nah?
+    so all this is is restriction of first && last as 1st suspected
+    also test:
+      0004 = 4
+      4000 = 4
+      123 = 3 (not 4, not 2)
+      so maybe just run twice: yes first no last, no first yes last?
+1231
+1244
+ 2 7 9 3 1
+ 2 7111112
+1
+1
+12
+12
+123
+124
+1234
+1246
+AEIOU
+ */
+var rob = function (nums) {
+  // return robii1(nums);
+  return robii2(nums);
+};
+
+function rob1(nums) {
+  // Ot(n) Os(n) solution:
+  if (nums.length < 3) return Math.max(...nums, 0);
+  const dp = new Array(nums.length + 1).fill(0);
+  // choices: choose or pass = recursive relationship
+  // or choices: chose previous or passed previous:
+  dp[1] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    // dp[i+1] because dp is off by 1
+    dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+  }
+  return dp[dp.length - 1];
+}
+
+function rob2(nums) {
+  // Ot(n) Os(1) solution:
+  if (nums.length < 3) return Math.max(...nums, 0);
+  let dp2 = 0;
+  let dp1 = nums[0];
+  let dp = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp = Math.max(dp, dp2 + nums[i], dp1);
+    dp2 = dp1;
+    dp1 = dp;
+  }
+  return dp;
+}
+
+function robii1(nums) {
+  if (nums.length < 2) return nums[0];
+  return Math.max(
+    rob1(nums.slice(1)), // TODO: refactor to avoid slice Ot(n)
+    rob1(nums.slice(0, nums.length - 1))
+  );
+}
+
+function robii2(nums) {
+  if (nums.length < 2) return nums[0];
+  return Math.max(
+    rob2(nums.slice(1)), // TODO: refactor to avoid slice Ot(n)
+    rob2(nums.slice(0, nums.length - 1))
+  );
+}
+
+module.exports = {
+  robii1,
+  robii2,
+};

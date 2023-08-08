21d9b173b4cc916236ff14a0b4e1fbb0556277b6
hchiam <hchiam@users.noreply.github.com>
2023-08-07 21:11:37 -0600

    make factorial consistently output BigInt

--- factorial.js
+++ factorial.js
@@ -1,5 +1,5 @@
 function factorial(num) {
-  if (num === 0 || num === 1) return 1;
+  if (num === 0 || num === 1) return BigInt(1);
   const start = num - 1;
   num = BigInt(num);
   for (let i = start; i > 1; i--) {

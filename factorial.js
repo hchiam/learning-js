c1c6c1f6193cf8377b4de0f0898d7be4adca2ca3
hchiam <hchiam@users.noreply.github.com>
2023-08-07 20:58:47 -0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,9 @@
+function factorial(num) {
+  if (num === 0 || num === 1) return 1;
+  const start = num - 1;
+  num = BigInt(num);
+  for (let i = start; i > 1; i--) {
+    num *= BigInt(i);
+  }
+  return num;
+}

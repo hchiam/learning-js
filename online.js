9979849033c6daedebe4b4e1e51406de3a8529b9
hchiam <hchiam@users.noreply.github.com>
2023-08-08 22:26:07 -0600

    more robust isOnline() function that defaults to true for older browsers

--- online.js
+++ online.js
@@ -1,5 +1,9 @@
-console.log(navigator.onLine);
+console.log(isOnline());
 
 window.addEventListener("online", function (e) {
   console.log("window.addEventListener('online'");
 });
+
+function isOnline() {
+  return !("onLine" in navigator) || navigator.onLine;
+}

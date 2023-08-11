b92f0fa4aa2fb17a165f218965eaa6fac2a26e8e
hchiam <hchiam@users.noreply.github.com>
2023-08-10 22:12:19 -0600

    figured out Ot(n^2) Os(n) step by step

--- more-like-interview-questions/palindromic-substrings.js
+++ more-like-interview-questions/palindromic-substrings.js
@@ -35,17 +35,35 @@ idea 2: HMW make use of previously-calculated palindromes?
         0111020105010201110 (add if not on #) = 17 = correct
 AEIO U
  */
-var countSubstrings = function (s) {
-  let count = s.length; // count individual characters
-  let string = "";
+function countSubstrings(s) {
+  // Ot(n^2) Os(n)
+  let string = transformStringToHandleEvenAndOddInOnePass(s); // Ot(n) Os(n)
+  const countCharacters = s.length;
+  return countCharacters + countWhileGrowingFromEachIndex(string); // Ot(n^2) Os(1)
+}
+
+function transformStringToHandleEvenAndOddInOnePass(s) {
+  // Ot(n) Os(n)
+  // aabacabaa -> #a#a#b#a#c#a#b#a#a#
+  const output = [];
   for (let letter of s) {
-    string += "#" + letter;
+    // Ot(n)
+    output.push("#");
+    output.push(letter);
   }
-  string += "#"; // #a#a#b#a#c#a#b#a#a#
+  output.push("#");
+  return output.join(""); // Ot(n) Os(n)
+}
+
+function countWhileGrowingFromEachIndex(string) {
+  // Ot(n^2) Os(1)
+  let count = 0;
   for (let i = 1; i < string.length - 1; i++) {
+    // Ot(n)
     let left = i - 1;
     let right = i + 1;
     while (left >= 0 && right < string.length) {
+      // Ot(n)
       if (string[left] !== string[right]) break;
       if (string[left] !== "#") count++; // ===
       left--;
@@ -53,8 +71,7 @@ var countSubstrings = function (s) {
     }
   }
   return count;
-};
-// #a#a#b#a#c#a#b#a#a#
+}
 
 module.exports = {
   countSubstrings,

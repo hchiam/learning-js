83ace43c0e00d30153da0bbb7f14d5b2bfa59c0a
hchiam <hchiam@users.noreply.github.com>
2023-08-22 20:28:34 -0600

    enableClickWhileRunningThisBookmarklet for highlightWhatYouClick.js

--- bookmarklets/highlightWhatYouClick.js
+++ bookmarklets/highlightWhatYouClick.js
@@ -1,7 +1,9 @@
 javascript: (function () {
+  window.enableClickWhileRunningThisBookmarklet = false;
+  console.log("enableClickWhileRunningThisBookmarklet = false");
   [...document.querySelectorAll("*")].forEach((x) =>
     x.addEventListener("click", function (e) {
-      e.stopPropagation();
+      if (!window.enableClickWhileRunningThisBookmarklet) e.stopPropagation();
       x.style.background = "gold";
       x.style.color = "black";
     })

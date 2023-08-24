a833895306adb7b2dc133e1d7686fbd69ecf86f6
hchiam <hchiam@users.noreply.github.com>
2023-08-24 17:20:31 -0600

    just toggle everything

--- bookmarklets/highlightWhatYouClick.js
+++ bookmarklets/highlightWhatYouClick.js
@@ -1,13 +1,10 @@
 javascript: (function () {
-  window.highlightWhatYouClick = window.highlightWhatYouClick
-    ? !window.highlightWhatYouClick
-    : true;
-  window.enableClickWhileRunningThisBookmarklet = false;
-  console.log("enableClickWhileRunningThisBookmarklet = false");
+  window.highlightWhatYouClick = !window.highlightWhatYouClick;
+  console.log("Run again to toggle.");
   [...document.querySelectorAll("*")].forEach((x) =>
     x.addEventListener("click", function (e) {
-      if (!window.enableClickWhileRunningThisBookmarklet) e.stopPropagation();
       if (window.highlightWhatYouClick) {
+        e.stopPropagation();
         x.style.background = "gold";
         x.style.color = "black";
       }

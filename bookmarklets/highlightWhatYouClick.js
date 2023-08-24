d5ae7228297bcbff0148c8a8b9a5ddd3f827f7a4
hchiam <hchiam@users.noreply.github.com>
2023-08-23 20:56:47 -0600

    enable toggling highlightWhatYouClick

--- bookmarklets/highlightWhatYouClick.js
+++ bookmarklets/highlightWhatYouClick.js
@@ -1,11 +1,16 @@
 javascript: (function () {
+  window.highlightWhatYouClick = window.highlightWhatYouClick
+    ? !window.highlightWhatYouClick
+    : true;
   window.enableClickWhileRunningThisBookmarklet = false;
   console.log("enableClickWhileRunningThisBookmarklet = false");
   [...document.querySelectorAll("*")].forEach((x) =>
     x.addEventListener("click", function (e) {
       if (!window.enableClickWhileRunningThisBookmarklet) e.stopPropagation();
-      x.style.background = "gold";
-      x.style.color = "black";
+      if (window.highlightWhatYouClick) {
+        x.style.background = "gold";
+        x.style.color = "black";
+      }
     })
   );
 })();

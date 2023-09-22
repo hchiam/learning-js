a0d6df69464c622680f36e16bb411f301dcd0d78
hchiam <hchiam@users.noreply.github.com>
2023-09-21 19:53:39 -0600

    make it easier to tell what will get highlighted with an outline on mouseover

--- bookmarklets/highlightWhatYouClick.js
+++ bookmarklets/highlightWhatYouClick.js
@@ -1,13 +1,26 @@
 javascript: (function () {
   window.highlightWhatYouClick = !window.highlightWhatYouClick;
   console.log("Run again to toggle.");
-  [...document.querySelectorAll("*")].forEach((x) =>
-    x.addEventListener("click", function (e) {
+  [...document.querySelectorAll("*")].forEach((x) => {
+    x.addEventListener("click", (e) => {
       if (window.highlightWhatYouClick) {
         e.stopPropagation();
         x.style.background = "gold";
         x.style.color = "black";
       }
-    })
-  );
+    });
+    let outline = x.style.outline;
+    x.addEventListener("mouseover", (e) => {
+      if (window.highlightWhatYouClick) {
+        e.stopPropagation();
+        x.style.outline = "solid 3px gold";
+      }
+    });
+    x.addEventListener("mouseout", (e) => {
+      if (window.highlightWhatYouClick) {
+        e.stopPropagation();
+        x.style.outline = outline;
+      }
+    });
+  });
 })();

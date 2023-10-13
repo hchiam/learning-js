0e0b9109ed68a01c370c9aebdbee0b80484d76b3
Howard Chiam <hchiam@users.noreply.github.com>
2023-10-12 20:47:03 -0600

    Update disable-object-property-access-sorta-pseudo.js

--- disable-object-property-access-sorta-pseudo.js
+++ disable-object-property-access-sorta-pseudo.js
@@ -9,10 +9,12 @@ Object.defineProperty(batman, 'secretIdentity', {
   configurable: false,
   set: function(password) {
     if (password === "'puter") {
-      console.log('nana');
+      console.log('nananananananananananananananananananananananana');
       console.log('PASSWORD ACCEPTED.');
       console.log("BATMAN'S SECRET IDENTITY:");
       console.log(realName);
+      console.log("https://www.youtube.com/watch?v=gZjjrlrce6A");
+      console.log('nananananananananananananananananananananananana');
     }
   },
 });

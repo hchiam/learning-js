36e872d8493a4c985e00cbe972705e156120b543
Howard Chiam <hchiam@users.noreply.github.com>
2023-10-12 20:48:49 -0600

    collapse output

--- disable-object-property-access-sorta-pseudo.js
+++ disable-object-property-access-sorta-pseudo.js
@@ -9,14 +9,14 @@ Object.defineProperty(batman, 'secretIdentity', {
   configurable: false,
   set: function(password) {
     if (password === "'puter") {
-      console.log('nananananananananananananananananananananananana');
+      console.groupCollapsed('nananananananananananananananananananananananana');
       console.log('PASSWORD ACCEPTED.');
       console.log("BATMAN'S SECRET IDENTITY:");
       console.log(realName);
       console.log("https://www.youtube.com/watch?v=gZjjrlrce6A");
-      console.log('nananananananananananananananananananananananana');
+      console.groupEnd('nananananananananananananananananananananananana');
     }
   },
 });
 
-console.log(batman.secretIdentity = "'puter");
+console.log(batman.secretIdentity = "puter");

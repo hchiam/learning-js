267a4737e6ca78cc6aacde0b2c65adf87ffa2d6e
Howard Chiam <hchiam@users.noreply.github.com>
2023-11-21 19:45:20 -0700

    Create xssSnippetToClipboard.js

---
+++ bookmarklets/xssSnippetToClipboard.js
@@ -0,0 +1,16 @@
+javascript: (function () {
+  var toClipboard = "<script>alert('xss');</script>";
+  copy(toClipboard);
+  function copy(text) {
+    var textarea = document.createElement("textarea");
+    selection = document.getSelection();
+    textarea.textContent = text;
+    document.body.appendChild(textarea);
+    selection.removeAllRanges();
+    textarea.select();
+    document.execCommand("copy");
+    selection.removeAllRanges();
+    document.body.removeChild(textarea);
+    alert(`Copied to clipboard:\n\n${text}`);
+  }
+})();

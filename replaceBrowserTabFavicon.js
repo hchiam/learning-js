740ff16312c57ffc7b6e2b7202501b3e465fd91d
hchiam <hchiam@users.noreply.github.com>
2024-01-09 21:43:46 -0700

    fix function name to match file name replaceBrowserTabFavicon.js

--- replaceBrowserTabFavicon.js
+++ replaceBrowserTabFavicon.js
@@ -1,10 +1,10 @@
 const iconUrl =
   "https://thenounproject.com/api/private/icons/1092604/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0";
-removeBrowserTabFavicon(iconUrl);
+replaceBrowserTabFavicon(iconUrl);
 
-// or just remove the icon with: removeBrowserTabFavicon();
+// or just remove the icon with: replaceBrowserTabFavicon('');
 
-function removeBrowserTabFavicon(replacementIcon) {
+function replaceBrowserTabFavicon(replacementIcon) {
   var link = document.querySelector("link[rel~='icon']");
   if (!link) {
     link = document.createElement("link");

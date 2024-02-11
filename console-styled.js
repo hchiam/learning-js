ded7a6abc16e319629bb43de9b4c529c5829e5f8
hchiam <hchiam@users.noreply.github.com>
2024-02-11 15:14:59 -0700

    get styled console.log to work for Chrome too

--- console-styled.js
+++ console-styled.js
@@ -1,4 +1,6 @@
 console.log(
   `some %cfancy%c text`,
-  "font-size: 15px; background: green; color: white;"
+  "font-size: 15px; background: green; color: white;",
+  "all: reset;"
 );
+// NOTE: the 2nd %c requires explicit styles in Chrome (Firefox seems to assume reset)

f4b4a8a66a501f66e0f2286790387df2655f45dc
Howard Chiam <hchiam@users.noreply.github.com>
2024-03-12 17:44:14 -0600

    getElementsByText -> getElementsByXPath (because same code inside function)

--- xpath_get_elements_by_text.js
+++ xpath_get_elements_by_text.js
@@ -4,10 +4,10 @@
 // var xpath = "//p[contains(text(),'Some text contained in a p tag')]";
 var xpath =
   "//*[contains(@class, 'kix-appview-editor-container')]//*[contains(text(),'some text')]";
-var elements = getElementsByText(xpath);
+var elements = getElementsByXPath(xpath);
 console.log(elements);
 
-function getElementsByText(xpath) {
+function getElementsByXPath(xpath) {
   var iterator = document.evaluate(
     xpath,
     document,
@@ -30,5 +30,5 @@ function getElementsByTextInGoogleDocInFirefox(text) {
     "//*[contains(@class, 'kix-appview-editor-container')]//*[contains(@class, 'kix-lineview') and contains(.//span, '" +
     text +
     "')]"; // "/parent::span/parent::div/parent::div[contains(@class, 'kix-lineview')]";
-  return getElementsByText(xpath);
+  return getElementsByXPath(xpath);
 }

ab1ff8c0304623d17cb094cc6b7adf22154cb11e
Howard Chiam <hchiam@users.noreply.github.com>
2024-05-02 15:53:10 -0600

    Create jiraTicketNameNumberSummaryToClipboard_4.js

---
+++ bookmarklets/jiraTicketNameNumberSummaryToClipboard_4.js
@@ -0,0 +1,31 @@
+javascript:(()=>{
+  var parent = document.querySelector(
+    '[data-testid="issue.views.issue-base.foundation.breadcrumbs.parent-issue.item"]'
+  )?.innerText;
+  var current = document.querySelector(
+    '[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]'
+  )?.innerText;
+  var isSubtask =
+    document.querySelector(
+      '[data-testid="issue.views.issue-base.foundation.change-issue-type.button"]'
+    ).ariaLabel == "Sub-task - Change issue type";
+  var tickets = isSubtask ? parent + " / " + current : current;
+  var summaryDescription = document.querySelector(
+    '[data-testid="issue.views.issue-base.foundation.summary.heading"]'
+  ).innerText;
+  var ssTitle = tickets + " : " + summaryDescription;
+  console.log(ssTitle);
+  copy(ssTitle);
+  function copy(text) {
+    var textarea = document.createElement("textarea");
+    selection = document.getSelection();
+    textarea.textContent = text;
+    document.body.appendChild(textarea);
+    selection.removeAllRanges();
+    textarea.select();
+    document.execCommand("copy");
+    selection.removeAllRanges();
+    document.body.removeChild(textarea);
+    alert(`Copied to clipboard:\n\n${text}`);
+  }
+})();

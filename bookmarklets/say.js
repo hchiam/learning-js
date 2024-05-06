942df6b06b21ebe1abca37529fcec772d080ca9f
Howard Chiam <hchiam@users.noreply.github.com>
2024-05-05 18:32:37 -0600

    Update say.js

--- bookmarklets/say.js
+++ bookmarklets/say.js
@@ -5,6 +5,12 @@ javascript: window.say = function (what, voiceLang, callback) {
     ? voices.filter((v) => v.lang === voiceLang)[0]
     : voices[0];
   utterance.onend = callback;
+  if (utterance.voice?.voiceURI) {
+    utterance.voiceURI = utterance.voice.voiceURI;
+  }
+  if (utterance.voice?.lang) {
+    utterance.lang = utterance.voice.lang;
+  }
   speechSynthesis.speak(utterance);
 };
 say(prompt("Say what?"));

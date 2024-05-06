d92e020411cf0ac136f8017a03e528f9fad8f884
Howard Chiam <hchiam@users.noreply.github.com>
2024-05-05 18:54:07 -0600

    Create uniqueObjectsByKey.js

---
+++ uniqueObjectsByKey.js
@@ -0,0 +1,14 @@
+/**
+Based on: https://stackoverflow.com/questions/15125920/how-to-get-distinct-values-from-an-array-of-objects-in-javascript
+Example:
+var array =
+  [
+    { "name": "Joe", "age": 17 },
+    { "name": "Bob", "age": 17 },
+    { "name": "Carl", "age": 35 }
+  ];
+console.log(uniqueObjectsByKey(array, 'age'));
+*/
+function uniqueObjectsByKey(arrayOfObjects, key) {
+  return [...new Map(array.map((item) => [item[key],item]))];
+}

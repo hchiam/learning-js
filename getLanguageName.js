f2cfa5b4882b6246a1b4ff2c6045811318f6ed14
Howard Chiam <hchiam@users.noreply.github.com>
2024-05-05 19:27:36 -0600

    handle error in getLanguageName.js

--- getLanguageName.js
+++ getLanguageName.js
@@ -5,5 +5,9 @@ getLanguageName('fr-FR'); // French (France)
 */
 function getLanguageName(langCode) {
   const translator = new Intl.DisplayNames([navigator.language],{type:'language'});
-  return translator.of(langCode.replace('_','-')); // e.g. Android Chrome uses _ instead of -
+  try {
+    return translator.of(langCode.replace('_','-')); // e.g. Android Chrome uses _ instead of -
+  } catch(e) {
+    return langCode;
+  }
 }

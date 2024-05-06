ed5a0a35dd2fb558d2605a4e1264b6550e3759ba
Howard Chiam <hchiam@users.noreply.github.com>
2024-05-05 18:46:03 -0600

    example in function comment - getLanguageName.js

--- getLanguageName.js
+++ getLanguageName.js
@@ -1,3 +1,8 @@
+/**
+Example:
+getLanguageName('fr-CA'); // Canadian French
+getLanguageName('fr-FR'); // French (France)
+*/
 function getLanguageName(langCode) {
   const translator = new Intl.DisplayNames([navigator.language],{type:'language'});
   return translator.of(langCode);

c3f5682f24108422dcf141b144b3ea0717ad9e1a
Howard Chiam <hchiam@users.noreply.github.com>
2024-07-08 17:17:40 -0600

    Create say.js

---
+++ say.js
@@ -0,0 +1,16 @@
+// https://github.com/hchiam/learning-js/blob/main/bookmarklets/say.js
+function say(what, voiceLang, callback) {
+  const utterance = new SpeechSynthesisUtterance(what);
+  const voices = speechSynthesis.getVoices();
+  utterance.voice = voiceLang
+    ? voices.filter((v) => v.lang === voiceLang)[0]
+    : voices[0];
+  utterance.onend = callback;
+  if (utterance.voice?.voiceURI) {
+    utterance.voiceURI = utterance.voice.voiceURI;
+  }
+  if (utterance.voice?.lang) {
+    utterance.lang = utterance.voice.lang;
+  }
+  speechSynthesis.speak(utterance);
+}

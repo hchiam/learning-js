209138b0effe931c08b1ce6b94c9db6fe03c3bf5
Howard Chiam <hchiam@users.noreply.github.com>
2024-07-16 15:49:01 -0600

    Create scrollToNextItem.js

---
+++ bookmarklets/scrollToNextItem.js
@@ -0,0 +1,26 @@
+javascript:(()=>{
+    const defaultSelector = '.row';
+    const selector = prompt('selector? default is ' + defaultSelector) || defaultSelector;
+    alert('hit s to scroll to next item');
+    const colours = ['red','blue'];
+    [...$$(selector)].map((x,i)=>{
+        x.style.outline=`solid ${colours[i%colours.length]} 3px`;
+        x.style.outlineOffset = '-4px';
+    });
+    const s = getNextScrollItem();
+    $('body').addEventListener('keyup',e=>{
+        if (e.key === 's') {
+            window.scrollTo({
+                top: s.next().value.offsetTop
+            });
+        }
+    });
+    function* getNextScrollItem() {
+        const x = [...$$(selector)];
+        for (let instance of x) {
+            yield instance;
+        }
+    }
+    function $(selector) { return document.querySelector(selector); }
+    function $$(selector) { return document.querySelectorAll(selector); }
+})();

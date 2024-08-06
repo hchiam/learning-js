8501b6462bd18291d2ccca7b19532fd0115ac3b1
Howard Chiam <hchiam@users.noreply.github.com>
2024-08-05 19:12:21 -0600

    Create highlightScrollWhatYouClick.js

---
+++ bookmarklets/highlightScrollWhatYouClick.js
@@ -0,0 +1,35 @@
+javascript: (function () {
+  window.highlightWhatYouClick = !window.highlightWhatYouClick;
+  console.log("Run again to toggle.");
+  [...document.querySelectorAll("*")].forEach((x) => {
+    x.addEventListener("click", (e) => {
+      if (window.highlightWhatYouClick) {
+        e.stopPropagation();
+        x.style.background = "gold";
+        x.style.color = "black";
+        x.nextElementSibling?.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+    let outline = x.style.outline;
+    x.addEventListener("mouseover", (e) => {
+      if (window.highlightWhatYouClick) {
+        e.stopPropagation();
+        x.style.outline = "solid 3px gold";
+      }
+    });
+    x.addEventListener("mouseout", (e) => {
+      if (window.highlightWhatYouClick) {
+        e.stopPropagation();
+        x.style.outline = outline;
+      }
+    });
+  });
+  function getScrollParent(node) {
+    if (node === null) return null;
+    if (node.scrollHeight > node.clientHeight) {
+      return node;
+    } else {
+      return getScrollParent(node.parentNode);
+    }
+  }
+})();

ded039cd4995dd3d7eb501994a5729867367d242
Howard Chiam <hchiam@users.noreply.github.com>
2024-08-19 17:32:24 -0600

    improved version of preventTrailingPunctuationWrap.js with character replacement

--- preventTrailingPunctuationWrap.js
+++ preventTrailingPunctuationWrap.js
@@ -1,10 +1,21 @@
 /**
+Example:
+const before = `qu'est-ce que c'est que ca? je ne sais pas : `;
+const after = preventTrailingPunctuationWrap(before);
+console.log(before !== after); // true
+*/
+function preventTrailingPunctuationWrap(text) {
+  return text.replace(/\s+(\p{P}\s*?)/gu, '\u00a0$1');
+  // return text.replace(/\s+(\p{P}\s*?)$/gu, '\u00a0$1');
+}
+
+/**
 Based on: https://www.freecodecamp.org/news/what-is-punct-in-regex-how-to-match-all-punctuation-marks-in-regular-expressions/
 
 Example:
 $('.test').innerHTML = preventTrailingPunctuationWrap(`qu'est-ce que c'est que ca? je ne sais pas<span style="white-space: nowrap;"> </span>: `.repeat(2));
 // qu'est-ce que c'est que ca? je ne sais pas<span style="white-space: nowrap;"> </span>: qu'est-ce que c'est que ca? je ne sais pas<span style="white-space: nowrap;"> </span>: 
 */
-function preventTrailingPunctuationWrap(text) {
+function OLD_preventTrailingPunctuationWrap(text) {
   return text.replace(/\s+(\p{P}\s+?)$/gu, '<span style="white-space: nowrap;"> </span>$1');
 }

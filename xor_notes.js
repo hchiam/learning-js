c5b6f5a527b10c0258aaa87e93e9b4f019b2322d
Howard Chiam <hchiam@users.noreply.github.com>
2024-10-03 16:37:12 -0600

    clarify object ^ null in xor_notes.js

--- xor_notes.js
+++ xor_notes.js
@@ -1,6 +1,5 @@
 // don't use ^ for XOR on objects - doesn't do what i originally thought: 
 a = {a:1}
 b = null
-console.log('i expect it to be true:', Boolean(a ^ b) === true);
-console.log('what it actually is false:', Boolean(a ^ b) === false);
-console.log('unlike true ^ false', Boolean(true ^ false) === true)
+// i'd think object ^ null would be true
+console.log('it actually is false:', Boolean(a ^ b) === false);

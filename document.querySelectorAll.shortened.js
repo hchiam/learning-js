ce197db65e215b7fcc123077869f6a39854a3fec
Howard Chiam <hchiam@users.noreply.github.com>
2024-11-12 19:23:20 -0700

    avoid having to do [...] in document.querySelectorAll.shortened.js

--- document.querySelectorAll.shortened.js
+++ document.querySelectorAll.shortened.js
@@ -1,5 +1,7 @@
 // $ = document.querySelectorAll; won't work
-$ = document.querySelectorAll.bind(document);
+$$ = document.querySelectorAll.bind(document);
 
 // or even shorter:
-$ = s => document.querySelectorAll(s);
+$$ = s => [...document.querySelectorAll(s)];
+
+$ = s => document.querySelector(s);

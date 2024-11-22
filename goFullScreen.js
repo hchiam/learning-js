a41514d62e4318b45b82da4a9c16287157743dcd
Howard Chiam <hchiam@users.noreply.github.com>
2024-11-21 20:16:11 -0700

    document.exitFullscreen();

--- goFullScreen.js
+++ goFullScreen.js
@@ -1,4 +1,6 @@
 document.body.addEventListener("click", (event) => {
   // requestFullscreen must be called within an event, not called top-level
   document.body.requestFullscreen();
+  // to undo: (WITHOUT body!)
+  // document.exitFullscreen();
 });

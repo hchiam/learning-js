22366a89aa6fdd172e9e3a461af22abd1cc40868
Howard Chiam <hchiam@users.noreply.github.com>
2025-01-03 13:48:29 -0700

    Create scaleToFitAvailableSpaceWithAspectRatio.js

---
+++ scaleToFitAvailableSpaceWithAspectRatio.js
@@ -0,0 +1,16 @@
+/**
+Scale width and height to fit the lesser-available width/height while maintaining an aspect ratio:
+*/
+function scaleToFit(availableW, availableH, aspectRatioW, aspectRatioH) {
+  let scale = 1;
+  const deltaW = Math.abs(availableW - aspectRatioW);
+  const deltaH = Math.abs(availableH - aspectRatioH);
+  if (deltaW < deltaH) {
+    scale = availableW / aspectRatioW;
+  } else {
+    scale = availableH / aspectRatioH;
+  }
+  const width = Math.floor(aspectRatioW * scale);
+  const height = Math.floor(aspectRatioH * scale);
+  return { width, height };
+}

b38096fa638236d6c89d58ddb35ed2c0f147159b
Howard Chiam <hchiam@users.noreply.github.com>
2025-01-23 23:25:51 -0700

    add playSound to beep.js

--- beep.js
+++ beep.js
@@ -8,3 +8,11 @@ function beep() {
 }
 
 beep();
+
+// or:
+
+function playSound(filePath, volume /* 0 to 1 */) {
+  const audio = new Audio(filePath);
+  if (volume) audio.volume = volume;
+  audio.play();
+}

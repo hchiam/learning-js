70c6d725aeac082cbc48e541d0d20b61bc1ff2c9
Howard Chiam <hchiam@users.noreply.github.com>
2025-01-28 15:51:31 -0700

    explain what 0 seconds in setTimeout is for

--- event-loop_call-stack_task-queue_microtask-queue.js
+++ event-loop_call-stack_task-queue_microtask-queue.js
@@ -1,3 +1,6 @@
+// https://www.youtube.com/watch?v=8aGhZQkoFbQ&t=896s = QUICK EXPLANATION:
+// use setTimeout with 0 seconds because event loop queue will be emptied after function call stack is clear
+
 // https://www.youtube.com/watch?v=FSs_JYwnAdI
 
 setTimeout(

242a7a7ff6db593a36b2b09fc5c4e03c3328a501
Howard Chiam <hchiam@users.noreply.github.com>
2025-02-11 11:31:26 -0700

    Update abort-fetch.js

--- abort-fetch.js
+++ abort-fetch.js
@@ -3,7 +3,7 @@ const signal = controller.signal;
 
 setTimeout(() => controller.abort(), 5000);
 
-fetch(url, { signal })
+fetch(url, { signal }) // if abort is already called, fetch won't bother running and wasting resources
   .then((response) => {
     return response.text();
   })
@@ -12,3 +12,39 @@ fetch(url, { signal })
   });
 
 // more here (like responding to abort): https://developers.google.com/web/updates/2017/09/abortable-fetch
+
+// https://www.youtube.com/watch?v=BeZfiCPhZbI
+
+// you can also write multiple removeEventListener calls more efficiently/cleanly:
+
+const signal = AbortSignal.timeout(1000);
+
+document.addEventListener('drag', ()=>{}, { signal });
+document.addEventListener('dragend', ()=>{}, { signal });
+document.addEventListener('dragstart', ()=>{}, { signal });
+document.addEventListener('dragover', ()=>{}, { signal });
+
+// and combine timeout abort with manually calling abort:
+
+const controller = new AbortController();
+const signal = AbortSignal.any(
+  [
+    controller.signal, // lets you manually abort
+    AbortSignal.timeout(1000), // will abort after 1 second
+  ]
+);
+
+document.addEventListener('drag', ()=>{}, { signal });
+document.addEventListener('dragend', ()=>{}, { signal });
+document.addEventListener('dragstart', ()=>{}, { signal });
+document.addEventListener('dragover', ()=>{}, { signal });
+
+controller.abort();
+
+// also there's a built-in "abort" event listener:
+
+signal.addEventListener('abort', ()=>{
+  //...
+});
+
+// see abortableTransaction.js for a helper function

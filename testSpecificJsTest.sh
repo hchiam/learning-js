cd5df717cbd6ea1f22c9993ea08de1940863b844
hchiam <hchiam@users.noreply.github.com>
2025-03-21 19:01:55 -0600

    add more notes about arguments to package.json to .sh

--- testSpecificJsTest.sh
+++ testSpecificJsTest.sh
@@ -1,5 +1,7 @@
+# this is also an example of passing an argument (parameter) to a package.json script, which runs in this .sh file with the argument as $@
+
 # for example: yarn test1 deepMerge
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then # $# is for the number of parameters
     echo "enter the first part of the JS file's name, for example: yarn test1 deepMerge"
 else
     jest --bail --findRelatedTests $@.test.js

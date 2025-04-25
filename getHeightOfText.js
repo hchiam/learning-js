94692ee3a33f3025cf04190b86907d0430ca71c2
hchiam <hchiam@users.noreply.github.com>
2025-04-24 19:59:16 -0600

    getHeightOfText.js

---
+++ getHeightOfText.js
@@ -0,0 +1,9 @@
+function getHeightOfText(text, font) {
+  const tempCanvas = document.createElement("canvas");
+  const ctx = tempCanvas.getContext("2d");
+  ctx.font = font ?? "1rem Arial";
+  const measurements = ctx.measureText(text);
+  return (
+    measurements.fontBoundingBoxAscent + measurements.fontBoundingBoxDescent
+  );
+}

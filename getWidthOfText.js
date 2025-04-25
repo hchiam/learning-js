5d1bf6ccf5591796e85f9759cbcf9263f14dc923
hchiam <hchiam@users.noreply.github.com>
2025-04-24 18:52:36 -0600

    getWidthOfText.js

---
+++ getWidthOfText.js
@@ -0,0 +1,6 @@
+function getWidthOfText(text, font) {
+  const tempCanvas = document.createElement("canvas");
+  const ctx = tempCanvas.getContext("2d");
+  ctx.font = font ?? "1rem Arial";
+  return ctx.measureText(text).width;
+}

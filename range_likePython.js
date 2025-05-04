59ff39b9f6e36487cc574c2dcd29fd4e148451fc
Howard Chiam <hchiam@users.noreply.github.com>
2025-05-04 15:39:36 -0600

    range_likePython.js: rangeArray with exclusive end

--- range_likePython.js
+++ range_likePython.js
@@ -1,21 +1,43 @@
 /**
+rangeArray(0, 20, 5); // [ 0, 5, 10, 15 ]
+
+for (const n of rangeArray(0, 20, 5)) {
+  console.log(n); // 0, 5, 10, 15
+}
+*/
+function rangeArray(start, end, step = 1) {
+  const output = [];
+
+  if (typeof end === 'undefined') {
+    end = start;
+    start = 0;
+  }
+
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
+
+  return output;
+};
+
+/**
 https://fireship.io/lessons/typescript-design-patterns/
 
 for (const n of range(0, 20, 5)) {
   console.log(n); // 5, 10, 15, 20
 }
 */
-function range(start, end, step=1) {
+function range(start, endInclusive, step=1) {
   return {
     [Symbol.iterator]() { // so you can use it with "for of"
       return this;
     },
     next() {
-      if (start < end) {
+      if (start < endInclusive) {
         start = start + step;
         return { done: false, value: start };
       }
-      return { done: true, value: end };
+      return { done: true, value: endInclusive };
     }
   };
 }

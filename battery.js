1c679a84c86a9c681508c2175efd95f9f17dffa5
hchiam <hchiam@users.noreply.github.com>
2025-05-29 19:24:36 -0600

    fix Firefox console error for battery.js (when not available)

--- battery.js
+++ battery.js
@@ -1,7 +1,6 @@
 function onLowBattery(callback, threshold = 0.2) {
   navigator?.getBattery?.().then((battery) => {
-    battery.addEventListener("levelchange", () => {
-      console.log(battery.level);
+    battery.addEventListener?.("levelchange", () => {
       if (callback && battery.level < threshold) {
         callback();
       }

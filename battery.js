e6c84618d97d3222a5ac1b40fce5c7d93cd83cf5
hchiam <hchiam@users.noreply.github.com>
2025-05-29 21:10:34 -0600

    handle if battery is already low on page load

--- battery.js
+++ battery.js
@@ -1,5 +1,8 @@
 function onLowBattery(callback, threshold = 0.2) {
   navigator?.getBattery?.().then((battery) => {
+    if (callback && battery.level < threshold) {
+      callback();
+    }
     battery.addEventListener?.("levelchange", () => {
       if (callback && battery.level < threshold) {
         callback();

1c5eaa9fa93b06306d8c2ae794bbdeda126b56b9
hchiam <hchiam@users.noreply.github.com>
2025-10-22 22:58:19 -0600

    CORRECTION: from hex to bits, NOW ACTUALLY use 4 digits

--- hexAndBinaryBits.js
+++ hexAndBinaryBits.js
@@ -2,19 +2,32 @@ function hexStringToBitString(hexStringWithCommasForNewRows) {
   // return parseInt(hexString, 16).toString(2);
   return hexStringWithCommasForNewRows
     .split(",")
-    .map((x) => parseInt(x, 16).toString(2).padStart(4, "0"))
+    .map((hexLetters) => {
+      const row = [];
+      hexLetters.split("").forEach((hexLetter) => {
+        row.push(...parseInt(hexLetter, 16).toString(2).padStart(4, "0"));
+      });
+      return row.join("");
+    })
     .join(",");
 }
 console.log(hexStringToBitString("a1,a,1")); // "10100001,1010,0001"
+console.log(hexStringToBitString("1a")); // "00011010"
 
 function hexStringToBitArray(hexStringWithCommasForNewRows) {
-  return hexStringWithCommasForNewRows.split(",").map((hexLetters) =>
-    parseInt(hexLetters, 16)
-      .toString(2)
-      .padStart(4, "0")
-      .split("")
-      .map((x) => (x === "1" ? 1 : 0))
-  );
+  return hexStringWithCommasForNewRows.split(",").map((hexLetters) => {
+    const row = [];
+    hexLetters.split("").forEach((hexLetter) => {
+      row.push(
+        ...parseInt(hexLetter, 16)
+          .toString(2)
+          .padStart(4, "0")
+          .split("")
+          .map((x) => (x === "1" ? 1 : 0))
+      );
+    });
+    return row;
+  });
 }
 console.log(hexStringToBitArray("a1,a,1")); // [[1,0,1,0,0,0,0,1],[1,0,1,0],[0,0,0,1]]
 

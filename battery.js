942cec77771735fc2d23b9a0f173edac5e70fb66
hchiam <hchiam@users.noreply.github.com>
2025-11-30 22:11:58 -0700

    added comment on availability and level passed in to callback

--- battery.js
+++ battery.js
@@ -1,11 +1,12 @@
-function onLowBattery(callback, threshold = 0.2) {
+/** limited availability https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getBattery */
+function onLowBattery(callback, threshold = 0.4) {
   navigator?.getBattery?.().then((battery) => {
     if (callback && battery.level < threshold) {
-      callback();
+      callback(battery.level);
     }
     battery.addEventListener?.("levelchange", () => {
       if (callback && battery.level < threshold) {
-        callback();
+        callback(battery.level);
       }
     });
   });

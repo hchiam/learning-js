d2b63b4812928081559e3abed705bd9ff32a1bc1
hchiam <hchiam@users.noreply.github.com>
2025-12-10 23:27:03 -0700

    battery.js has backup callback

--- battery.js
+++ battery.js
@@ -1,13 +1,28 @@
 /** limited availability https://developer.mozilla.org/en-US/docs/Web/API/Navigator/getBattery */
-function onLowBattery(callback, threshold = 0.4) {
-  navigator?.getBattery?.().then((battery) => {
-    if (callback && battery.level < threshold) {
-      callback(battery.level);
-    }
-    battery.addEventListener?.("levelchange", () => {
+function onLowBattery(callback, threshold = 0.4, backupCallback) {
+  if (!navigator?.getBattery) {
+    runBatteryCheckBackup(backupCallback);
+  } else {
+    navigator.getBattery().then((battery) => {
       if (callback && battery.level < threshold) {
         callback(battery.level);
+        battery.addEventListener?.("levelchange", () => {
+          if (callback && battery.level < threshold) {
+            callback(battery.level);
+          }
+        });
+      } else {
+        runBatteryCheckBackup(backupCallback);
       }
     });
-  });
+  }
+}
+
+function runBatteryCheckBackup(backupCallback) {
+  const minutes = 30;
+  const seconds = minutes * 60;
+  const ms = seconds * 1000;
+  setInterval(() => {
+    if (backupCallback) backupCallback();
+  }, ms);
 }

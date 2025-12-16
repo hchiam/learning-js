40fc38c19f560af153629e1a317cd15e9004d0be
Howard Chiam <hchiam@users.noreply.github.com>
2025-12-16 09:01:41 -0700

    onDeviceOrientationChange window.screen.orientation change vs orientationchange and deviceorientation

--- get-device-orientation-and-motion.js
+++ get-device-orientation-and-motion.js
@@ -1,11 +1,19 @@
+// my suggestion: don't use deprecated 'orientationchange' event, and dont use 'deviceorientation' event unless you need it to fire constantly/often
+
+function onDeviceOrientationChange(callback) {
+  window.screen.orientation.addEventListener('change', callback);
+}
+
 // more info:
 // https://web.dev/devices-introduction
 // https://developers.google.com/web/fundamentals/native-hardware/device-orientation
 // https://developer.chrome.com/docs/devtools/device-mode/#orientation
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/orientationchange_event
+// https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event
 
 if (window.DeviceOrientationEvent) {
   window.addEventListener(
-    "deviceorientation",
+    "deviceorientation", // first "constantly" (because "always" detecting)
     function deviceOrientationHandler(event) {
       console.log("deviceOrientationHandler", event);
     },
